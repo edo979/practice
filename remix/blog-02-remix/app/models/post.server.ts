1e7c7a5ed63c293a9f71d048bab8e654f6281252
edo979 <prezentabilan@gmail.com>
2022-10-31 12:49:55 +0100

    edit post

--- remix/blog-02-remix/app/models/post.server.ts
+++ remix/blog-02-remix/app/models/post.server.ts
@@ -11,8 +11,22 @@ export async function getFirstPost() {
   return await prisma.post.findFirst()
 }
 
+export async function getPost(slug: string) {
+  return await prisma.post.findFirst({ where: { slug: slug } })
+}
+
 export async function createPost(
   post: Pick<Post, 'slug' | 'title' | 'markdown'>
 ) {
   return await prisma.post.create({ data: post })
+}
+
+export async function updatePost(
+  slug: string,
+  post: Pick<Post, 'slug' | 'title' | 'markdown'>
+) {
+  return await prisma.post.update({
+    where: { slug: slug },
+    data: { title: post.title, slug: post.slug, markdown: post.markdown },
+  })
 }
