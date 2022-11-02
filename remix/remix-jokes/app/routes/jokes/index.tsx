37f03868d38ce2223e625557d983f811caaa7532
edo979 <prezentabilan@gmail.com>
2022-11-02 19:47:46 +0100

    add logout file, add error handling

--- remix/remix-jokes/app/routes/jokes/index.tsx
+++ remix/remix-jokes/app/routes/jokes/index.tsx
@@ -1,6 +1,6 @@
 import { Joke } from '@prisma/client'
 import { json, LoaderFunction } from '@remix-run/node'
-import { Link, useLoaderData } from '@remix-run/react'
+import { Link, useLoaderData, useCatch } from '@remix-run/react'
 import { db } from '~/utils/db.server'
 
 type LoaderData = {
@@ -14,6 +14,12 @@ export const loader: LoaderFunction = async () => {
     take: 1,
     skip: randomRowNumber,
   })
+
+  if (!randomJoke) {
+    throw new Response('No random joke found', {
+      status: 404,
+    })
+  }
 
   const data: LoaderData = { randomJoke }
   return json(data)
@@ -29,4 +35,17 @@ export default function JokesIndexRoute() {
       <Link to={randomJoke.id}>"{randomJoke.name}" Permalink</Link>
     </div>
   )
+}
+
+export function CatchBoundary() {
+  const caught = useCatch()
+
+  if (caught.status === 404) {
+    return <div className="error-container">There are no jokes to display.</div>
+  }
+  throw new Error(`Unexpected caught response with status: ${caught.status}`)
+}
+
+export function ErrorBoundary() {
+  return <div className="error-container">I did a whoopsies.</div>
 }
