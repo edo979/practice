7e7de3eafc393053098d3abc31ed7a5d63df0438
edo979 <prezentabilan@gmail.com>
2022-11-03 20:20:45 +0100

    validate username and password

--- remix/remix-jokes-01/app/routes/login.tsx
+++ remix/remix-jokes-01/app/routes/login.tsx
@@ -1,8 +1,27 @@
 import { ActionFunction, json } from '@remix-run/node'
 import { useActionData } from '@remix-run/react'
+import classNames from 'classnames'
 
 type ActionData = {
   formError?: string
+  fieldErrors?: {
+    username: string | undefined
+    password: string | undefined
+  }
+  fields?: {
+    username: string
+    password: string
+  }
+}
+
+function validateUsername(username: unknown) {
+  if (typeof username !== 'string' || username.length < 3)
+    return `Usernames must be at least 3 characters long`
+}
+
+function validatePassword(password: unknown) {
+  if (typeof password !== 'string' || password.length < 6)
+    return `passwords must be at least 6 characters long`
 }
 
 const badRequest = (data: ActionData) => json(data, { status: 400 })
@@ -15,6 +34,16 @@ export const action: ActionFunction = async ({ request }) => {
   if (typeof username !== 'string' || typeof password !== 'string') {
     return badRequest({ formError: 'Form not submittet correctly' })
   }
+
+  const fields = { username, password }
+  const fieldErrors = {
+    username: validateUsername(username),
+    password: validatePassword(password),
+  }
+
+  if (Object.values(fieldErrors).some(Boolean))
+    return badRequest({ fieldErrors, fields })
+
   return badRequest({})
 }
 
@@ -33,15 +62,16 @@ export default function Login() {
               </label>
               <input
                 type="text"
-                className={`form-control ${
-                  actionData?.formError && 'is-invalid'
-                }`}
+                className={classNames('form-control', {
+                  'is-invalid': actionData?.fieldErrors?.username,
+                })}
                 id="username"
                 name="username"
-                aria-describedby="validationFeedback"
+                defaultValue={actionData?.fields?.username}
+                aria-describedby="validationUsernameFeedback"
               />
-              <div className="invalid-feedback" id="validationFeedback">
-                {actionData?.formError}
+              <div className="invalid-feedback" id="validationUsernameFeedback">
+                {actionData?.fieldErrors?.username}
               </div>
             </div>
 
@@ -51,11 +81,19 @@ export default function Login() {
               </label>
               <input
                 type="password"
-                className="form-control"
+                className={classNames('form-control', {
+                  'is-invalid': actionData?.fieldErrors?.password,
+                })}
                 id="password"
                 name="password"
+                defaultValue={actionData?.fields?.password}
+                aria-describedby="validationPasswordFeedback"
               />
+              <div className="invalid-feedback" id="validationPasswordFeedback">
+                {actionData?.fieldErrors?.password}
+              </div>
             </div>
+
             {actionData?.formError && (
               <div className="invalid-feedback d-block">
                 {actionData?.formError}
