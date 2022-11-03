04fca8e9a300c1fb41511df4e507f9f56c169f69
edo979 <prezentabilan@gmail.com>
2022-11-03 14:54:53 +0100

    user login check input field is string

--- remix/remix-jokes-01/app/routes/login.tsx
+++ remix/remix-jokes-01/app/routes/login.tsx
@@ -1,22 +1,47 @@
+import { ActionFunction, json } from '@remix-run/node'
+import { useActionData } from '@remix-run/react'
+
+type ActionData = {
+  formError?: string
+}
+
+const badRequest = (data: ActionData) => json(data, { status: 400 })
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData()
+  const username = formData.get('username')
+  const password = formData.get('password')
+
+  if (typeof username !== 'string' || typeof password !== 'string') {
+    return badRequest({ formError: 'Form not submittet correctly' })
+  }
+  return badRequest({})
+}
+
 export default function Login() {
+  const actionData = useActionData<ActionData>()
+  console.log(actionData)
+
   return (
     <div className="container">
       <div className="row">
         <div className="col">
-          <form action="post">
+          <form method="post">
             <div className="mb-3">
               <label htmlFor="username" className="form-label">
                 Email address
               </label>
               <input
                 type="text"
-                className="form-control"
+                className={`form-control ${
+                  actionData?.formError && 'is-invalid'
+                }`}
                 id="username"
                 name="username"
-                aria-describedby="emailHelp"
+                aria-describedby="validationFeedback"
               />
-              <div id="emailHelp" className="form-text">
-                We'll never share your email with anyone else.
+              <div className="invalid-feedback" id="validationFeedback">
+                {actionData?.formError}
               </div>
             </div>
 
@@ -31,6 +56,14 @@ export default function Login() {
                 name="password"
               />
             </div>
+            {actionData?.formError && (
+              <div className="invalid-feedback d-block">
+                {actionData?.formError}
+              </div>
+            )}
+            <button className="btn btn-primary" type="submit">
+              Login
+            </button>
           </form>
         </div>
       </div>
