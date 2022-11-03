e4560616e8996dbbaaee98bee0f22f209da21db1
edo979 <prezentabilan@gmail.com>
2022-11-03 11:27:14 +0100

    start new jokes project

---
+++ remix/remix-jokes-01/app/routes/index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>remix Jokes</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
