b75cf9697edb4118d7a260e4f3737648cd6bbce9
edo979 <prezentabilan@gmail.com>
2022-11-12 19:13:50 +0100

    add pagination

--- remix/remix-jokes-01/app/routes/admin/jokes.tsx
+++ remix/remix-jokes-01/app/routes/admin/jokes.tsx
@@ -1,9 +1,5 @@
 import { Outlet } from '@remix-run/react'
 
 export default function AdminJokesRoute() {
-  return (
-    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 my-0">
-      <Outlet />
-    </div>
-  )
+  return <Outlet />
 }
--- remix/remix-jokes-01/app/routes/admin/jokes/index.tsx
+++ remix/remix-jokes-01/app/routes/admin/jokes/index.tsx
@@ -1,35 +1,115 @@
-import { Joke } from '@prisma/client'
+import { Joke, prisma } from '@prisma/client'
 import { LoaderFunction, json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import {
+  Link,
+  useLoaderData,
+  useParams,
+  useSearchParams,
+} from '@remix-run/react'
+import classNames from 'classnames'
 import JokeComponent from '~/components/joke'
 import { db } from '~/utils/db.server'
 import { requireUserId } from '~/utils/session.server'
 
 type LoaderData = {
   jokes: Joke[]
+  paginationData: {
+    page: number
+    numOfJokesToShow: number
+    jokeCount: number
+  }
 }
 export const loader: LoaderFunction = async ({ request }) => {
-  const userId = await requireUserId(request)
+  // Pagination page
+  const page = parseInt(new URL(request.url).searchParams.get('page') ?? '0')
+  const numOfJokesToShow = 5
 
+  // Load jokes
+  const userId = await requireUserId(request)
+  const jokeCount = await db.joke.count({ where: { jokesterId: userId } })
   const jokes = await db.joke.findMany({
     where: { jokesterId: userId },
-    take: 5,
+    skip: page * numOfJokesToShow,
+    take: numOfJokesToShow,
     orderBy: { createdAt: 'desc' },
   })
 
-  return json({ jokes } as LoaderData)
+  const paginationData = { page, numOfJokesToShow, jokeCount }
+
+  return json({ jokes, paginationData } as LoaderData)
 }
 
 export default function AdminJokeRoute() {
-  const { jokes } = useLoaderData<LoaderData>()
+  const {
+    jokes,
+    paginationData: { page, numOfJokesToShow, jokeCount },
+  } = useLoaderData<LoaderData>()
+  const [searchParams] = useSearchParams()
+  const currentPage = parseInt(searchParams.get('page') ?? '0')
+
+  const numOfPages = Math.ceil(jokeCount / numOfJokesToShow)
+  const paginationPages = []
+  for (let i = 0; i < numOfPages; i++) {
+    paginationPages.push(
+      <li className="page-item" key={i}>
+        <Link
+          to={`.?page=${i}`}
+          className={classNames('page-link', { active: currentPage === i })}
+          aria-current={currentPage === i ? 'page' : 'false'}
+        >
+          {i + 1}
+        </Link>
+      </li>
+    )
+  }
+
+  const prevPage = () => {
+    const toPage = currentPage - 1
+    if (toPage < 0) return 0
+    return toPage
+  }
+
+  const nextPage = () => {
+    const toPage = currentPage + 1
+    if (toPage >= numOfPages) return numOfPages - 1 // index 0 based
+    return toPage
+  }
 
   return (
     <>
-      {jokes.map((joke) => (
-        <div key={joke.id} className="col">
-          <JokeComponent {...joke} />
-        </div>
-      ))}
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 my-0">
+        {jokes.map((joke) => (
+          <div key={joke.id} className="col">
+            <JokeComponent {...joke} />
+          </div>
+        ))}
+      </div>
+
+      <nav aria-label="Joke pagination">
+        <ul className="pagination justify-content-center mt-5">
+          <li className="page-item">
+            <Link
+              to={`.?page=${prevPage()}`}
+              className="page-link"
+              aria-label="Previous"
+            >
+              <span aria-hidden="true">&laquo;</span>
+            </Link>
+          </li>
+
+          {paginationPages}
+
+          <li className="page-item">
+            <Link
+              to={`.?page=${nextPage()}`}
+              className="page-link"
+              aria-label="Next"
+            >
+              <span aria-hidden="true">&raquo;</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
