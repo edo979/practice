fe1141a55e260e6321241c8df9fae1b08307ff72
edo979 <prezentabilan@gmail.com>
2022-11-13 18:24:23 +0100

    finish node server practice

--- node/index.js
+++ node/index.js
@@ -3,35 +3,54 @@ const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'index.html'),
-      (err, content) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/html' })
-          res.end('<h1>Server Error</h1>')
-        }
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  )
 
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(content)
-      }
-    )
+  console.log(req.url)
+
+  let extname = path.extname(filePath)
+  let contentType = 'text/html'
+
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript'
+      break
+    case '.css':
+      contentType = 'text/css'
+      break
+    case '.json':
+      contentType = 'application/json'
+      break
+    case '.png':
+      contentType = 'image/png'
+      break
+    case '.jpg':
+      contentType = 'image/jpg'
+      break
   }
 
-  if (req.url === '/about') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'about.html'),
-      (err, content) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/html' })
-          res.end('<h1>Server Error!</h1>')
-        }
-
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(content)
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.readFile(
+          path.join(__dirname, 'public', '404.html'),
+          (err, content) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(content, 'utf8')
+          }
+        )
+      } else {
+        res.writeHead(500)
+        res.end(`Server Error: ${err.code}`)
       }
-    )
-  }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType })
+      res.end(content, 'utf8')
+    }
+  })
 })
 
 const PORT = process.env.PORT || 5000
