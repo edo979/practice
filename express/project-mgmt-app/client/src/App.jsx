ce01c98c225d211ac6dc05d14d5225eda37f04ff
edo979 <prezentabilan@gmail.com>
2022-11-15 19:31:58 +0100

    add header component, add style

--- express/project-mgmt-app/client/src/App.jsx
+++ express/project-mgmt-app/client/src/App.jsx
@@ -1,5 +1,14 @@
+import Header from './components/Header'
+
 function App() {
-  return <div className="">app</div>
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
 }
 
 export default App
