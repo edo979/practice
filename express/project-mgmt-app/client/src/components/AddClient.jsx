885d557cf7b8bbcfae8778cb9afbeadb50335a63
edo979 <prezentabilan@gmail.com>
2022-11-16 21:42:07 +0100

    add clients to db

--- express/project-mgmt-app/client/src/components/AddClient.jsx
+++ express/project-mgmt-app/client/src/components/AddClient.jsx
@@ -1,7 +1,40 @@
 import { FaUser } from 'react-icons/fa'
 import { useMutation } from '@apollo/client'
+import { ADD_CLIENT } from '../mutations/clientMutations'
+import { GET_CLIENTS } from '../queries/ClientQueries'
+import { useState } from 'react'
 
 export default function AddClient() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS })
+
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: [...clients, addClient] },
+      })
+    },
+  })
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (name === '' || phone === '' || email === '') {
+      return alert('Please fill in all fields')
+    }
+
+    addClient(name, email, phone)
+
+    setName('')
+    setEmail('')
+    setPhone('')
+  }
+
   return (
     <>
       <>
@@ -37,7 +70,7 @@ export default function AddClient() {
                 ></button>
               </div>
               <div className="modal-body">
-                <form>
+                <form onSubmit={onSubmit}>
                   <div className="mb-3">
                     <label htmlFor="name" className="form-label">
                       Name
@@ -46,8 +79,8 @@ export default function AddClient() {
                       type="text"
                       className="form-control"
                       id="name"
-                      // value={name}
-                      // onChange={(e) => setName(e.target.value)}
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
                     />
                   </div>
                   <div className="mb-3">
@@ -58,8 +91,8 @@ export default function AddClient() {
                       type="email"
                       className="form-control"
                       id="email"
-                      // value={email}
-                      // onChange={(e) => setEmail(e.target.value)}
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
                     />
                   </div>
                   <div className="mb-3">
@@ -70,8 +103,8 @@ export default function AddClient() {
                       type="text"
                       className="form-control"
                       id="phone"
-                      //value={phone}
-                      // onChange={(e) => setPhone(e.target.value)}
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
                     />
                   </div>
 
