f78514e3fd26062921966c09957a7ff739bc23cf
edo979 <prezentabilan@gmail.com>
2022-11-21 19:10:42 +0100

    instal client depedencies, basic UI, set new endpoint

--- express/flashcardsaga/server/src/index.ts
+++ express/flashcardsaga/server/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
 import connectDB from './config/db'
 import Deck from './model/Deck'
 import { config } from 'dotenv'
@@ -6,8 +7,14 @@ import { config } from 'dotenv'
 config()
 const PORT = process.env.PORT || 5000
 const app = express()
+app.use(cors())
 app.use(express.json())
 connectDB()
+
+app.get('/decks', async (req: Request, res: Response) => {
+  const decks = await Deck.find()
+  res.json(decks)
+})
 
 app.post('/decks', async (req: Request, res: Response) => {
   const newDeck = new Deck({
