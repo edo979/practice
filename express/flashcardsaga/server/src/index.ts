46243eeb35dda9834cafd89a44201941779cf400
edo979 <prezentabilan@gmail.com>
2022-11-21 12:42:54 +0100

    add mongo, add model, save model to db

--- express/flashcardsaga/server/src/index.ts
+++ express/flashcardsaga/server/src/index.ts
@@ -1,9 +1,21 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import connectDB from './config/db'
+import Deck from './model/Deck'
+import { config } from 'dotenv'
 
+config()
+const PORT = process.env.PORT || 5000
 const app = express()
+app.use(express.json())
+connectDB()
 
-app.get('/', (req, res) => {
-  res.send('Hello')
+app.post('/decks', async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  })
+  const createdDeck = await newDeck.save()
+
+  res.json(createdDeck)
 })
 
-app.listen(5000, () => console.log('Server started'))
+app.listen(PORT, () => console.log(`Server started on ${PORT}`))
