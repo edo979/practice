aca67a8fb1f3355b14ac178ab8064f32de9565ee
edo979 <prezentabilan@gmail.com>
2022-11-23 11:38:33 +0100

    add form for notes

--- react/noteApp/src/NewNote.tsx
+++ react/noteApp/src/NewNote.tsx
@@ -1,3 +1,10 @@
+import NoteForm from './NoteForm'
+
 export default function NewNote() {
-  return <div>NewNote</div>
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm />
+    </>
+  )
 }
