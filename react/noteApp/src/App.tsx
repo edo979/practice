ce2fbcbe8c5b5abdc996e1e96b2482c52949831b
edo979 <prezentabilan@gmail.com>
2022-11-23 12:17:13 +0100

    use locale storage, save notes data

--- react/noteApp/src/App.tsx
+++ react/noteApp/src/App.tsx
@@ -1,9 +1,48 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useMemo, useState } from 'react'
 import { Container } from 'react-bootstrap'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import NewNote from './NewNote'
+import { useLocalStorage } from './useLocalStorage'
+
+export type Note = {
+  id: string
+} & NoteData
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
+
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
+  const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      }
+    })
+  }, [notes, tags])
+
   return (
     <Container className="my-4">
       <Routes>
