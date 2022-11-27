079093a54b7369c53aa8f315fd5cc0df818f36c6
edo979 <prezentabilan@gmail.com>
2022-11-27 12:11:20 +0100

    create new models, save note to db

--- express/note-app/server/src/index.ts
+++ express/note-app/server/src/index.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from 'express'
 import { config } from 'dotenv'
+import connectDB from './config/db'
+import Note from './model/Note'
 
 config()
 const PORT = process.env.PORT || 5000
 const app = express()
+app.use(express.json())
+connectDB()
 
 // Index
 app.get('/', (req: Request, res: Response) => {
@@ -17,7 +21,14 @@ app.get('/notes', (req: Request, res: Response) => {
 
 // Add note
 app.post('/notes', (req: Request, res: Response) => {
-  res.send('Add note')
+  const newNote = new Note({
+    title: req.body.title,
+    body: req.body.body,
+  })
+
+  newNote.save()
+
+  res.json(newNote)
 })
 
 app.listen(PORT, () => console.log(`Server listenint on port: ${PORT}`))
