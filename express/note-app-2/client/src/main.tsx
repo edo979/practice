096e7e37fb45b0a487b2398007db09ba1a956d85
edo979 <prezentabilan@gmail.com>
2022-12-06 20:08:36 +0100

    add login and register route

--- express/note-app-2/client/src/main.tsx
+++ express/note-app-2/client/src/main.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
 import Home from './routes/Home'
+import Login from './routes/user/Login'
+import Register from './routes/user/Register'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'login', element: <Login /> },
+      { path: 'register', element: <Register /> },
+    ],
   },
 ])
 
---
+++ express/note-app-2/client/src/routes/user/Login.tsx
@@ -0,0 +1,42 @@
+import '../../styles/login-form.css'
+
+export default function Login() {
+  return (
+    <div className="form-signin">
+      <form>
+        {/* <img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> */}
+        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div className="form-floating">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label htmlFor="floatingInput">Email address</label>
+        </div>
+
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
+
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me" /> Remember me
+          </label>
+        </div>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">
+          Sign in
+        </button>
+        <p className="mt-5 mb-3 text-muted">© 2017–2022</p>
+      </form>
+    </div>
+  )
+}
