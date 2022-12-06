7d0a43d7fb6c483b7921325cdd11571506f69fbe
edo979 <prezentabilan@gmail.com>
2022-12-06 11:04:57 +0100

    add client

---
+++ express/note-app-2/client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">App</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
