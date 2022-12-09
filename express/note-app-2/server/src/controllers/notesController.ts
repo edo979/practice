cf452797a31389a4dc4f444a9e16dd09cb26252d
edo979 <prezentabilan@gmail.com>
2022-12-09 19:18:40 +0100

    query for user notes

--- express/note-app-2/server/src/controllers/notesController.ts
+++ express/note-app-2/server/src/controllers/notesController.ts
@@ -15,8 +15,16 @@ export const notesController = {
     try {
       const userId = req.session.userId
       const notes = await User.findById(userId)
-        .populate('notes', ['tags', 'title', 'body'])
-        .select('userName')
+        .populate({
+          path: 'notes',
+          select: '-_id title body tags',
+          populate: {
+            path: 'tags',
+            select: '-_id label',
+          },
+        })
+        .select('-_id userName notes')
+
       res.json(notes)
     } catch (error) {
       res.status(500).send({ message: 'Could not get Notes.' })
