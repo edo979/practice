541b02381f0bae8e8d0fbde94c0d6daf63b898ce
edo979 <prezentabilan@gmail.com>
2022-12-12 12:40:41 +0100

    working on note layout

--- express/note-app-2/client/src/routes/notes/Note.tsx
+++ express/note-app-2/client/src/routes/notes/Note.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunction, redirect, useLoaderData } from 'react-router-dom'
+import { Link, LoaderFunction, redirect, useLoaderData } from 'react-router-dom'
 import { Note as NoteModel } from './NotesList'
 
 type LoaderData = {
@@ -18,9 +18,8 @@ export const loader: LoaderFunction = async ({ request, params }) => {
   )
 
   if (!res.ok) {
-    if (res.status >= 500) {
-      throw new Error('Server Error')
-    }
+    if (res.status >= 500) throw new Error('Server Error')
+    if (res.status === 404) throw new Error("That note doesn't exist!")
     return redirect('/login')
   }
 
@@ -32,5 +31,27 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 export default function Note() {
   const { note } = useLoaderData() as LoaderData
 
-  return <div>Note</div>
+  return (
+    <article>
+      <header className="hstack align-items-center justify-content-between">
+        <h1>{note.title}</h1>
+        <nav className="hstack gap-1">
+          <Link to="..">
+            <button className="btn btn-secondary">Back</button>
+          </Link>
+          <Link to="..">
+            <button className="btn btn-primary">Edit</button>
+          </Link>
+        </nav>
+      </header>
+      <aside className="hstack gap-1">
+        {note.tags.map((tag) => (
+          <span key={tag.label} className="badge bg-primary">
+            {tag.label}
+          </span>
+        ))}
+      </aside>
+      <main className="mt-4">{note.body}</main>
+    </article>
+  )
 }
