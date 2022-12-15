76aa81d1daeffc07b837d52c5c3ad56b05045745
edo979 <prezentabilan@gmail.com>
2022-12-15 12:32:59 +0100

    working on home route

--- express/note-app-2/client/src/routes/Home.tsx
+++ express/note-app-2/client/src/routes/Home.tsx
@@ -20,21 +20,28 @@ export default function Home() {
   return (
     <section className="row g-4 py-5 row-cols-1 row-cols-lg-3">
       <article className="feature col">
-        <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-          {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg> */}
+        <div
+          className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3"
+          style={{ width: '4rem', height: '4rem', borderRadius: '0.75rem' }}
+        >
+          <svg
+            className="bi"
+            width="1em"
+            height="1em"
+            style={{ verticalAlign: '-0.125em', fill: 'currentColor' }}
+          >
+            <use xlinkHref="#people-circle"></use>
+          </svg>
         </div>
-        <h3 className="fs-2">User</h3>
+        <h3 className="fs-2">Users</h3>
         <p>
-          Paragraph of text beneath the heading to explain the heading. We'll
-          add onto it with another sentence and probably just keep going until
-          we run out of words.
+          We have {usersCount} registered users. Paragraph of text beneath the
+          heading to explain the heading. We'll add onto it with another
+          sentence and probably just keep going until we run out of words.{' '}
         </p>
-        <a href="#" className="icon-link d-inline-flex align-items-center">
-          Call to action
-          {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#chevron-right"></use></svg> */}
-        </a>
       </article>
-      <div className="feature col">
+
+      <article className="feature col">
         <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
           {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg> */}
         </div>
@@ -44,12 +51,9 @@ export default function Home() {
           add onto it with another sentence and probably just keep going until
           we run out of words.
         </p>
-        <a href="#" className="icon-link d-inline-flex align-items-center">
-          Call to action
-          {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#chevron-right"></use></svg> */}
-        </a>
-      </div>
-      <div className="feature col">
+      </article>
+
+      <article className="feature col">
         <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
           {/* <svg className="bi" width="1em" height="1em">
               <use xlink:href="#toggles2"></use>
@@ -61,11 +65,7 @@ export default function Home() {
           add onto it with another sentence and probably just keep going until
           we run out of words.
         </p>
-        <a href="#" className="icon-link d-inline-flex align-items-center">
-          Call to action
-          {/* <svg className="bi" width="1em" height="1em"><use xlink:href="#chevron-right"></use></svg> */}
-        </a>
-      </div>
+      </article>
     </section>
   )
 }
