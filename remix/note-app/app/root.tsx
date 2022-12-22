a1b0a8d1f550d52468b641594838769ce75ac285
edo979 <prezentabilan@gmail.com>
2022-12-22 19:39:59 +0100

    fix bootstrap preload warning in console

--- remix/note-app/app/root.tsx
+++ remix/note-app/app/root.tsx
@@ -11,13 +11,15 @@ import {
 export const links: LinksFunction = () => {
   return [
     {
-      rel: 'stylesheet',
+      as: 'style',
+      rel: 'stylesheet preload prefetch',
       href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css',
       integrity:
         'sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65',
       crossOrigin: 'anonymous',
     },
     {
+      as: 'script',
       rel: 'modulepreload',
       href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js',
       integrity:
