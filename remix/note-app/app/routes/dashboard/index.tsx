8a9fd47a173280d65e7edfbc3e6f6a7dc7b63982
edo979 <prezentabilan@gmail.com>
2022-12-25 19:27:34 +0100

    delete logout button

--- remix/note-app/app/routes/dashboard/index.tsx
+++ remix/note-app/app/routes/dashboard/index.tsx
@@ -46,9 +46,30 @@ export default function DashboardIndexRoute() {
         />
         <div className="navbar-nav">
           <div className="nav-item text-nowrap">
-            <a className="nav-link px-3" href="#">
-              Sign out
-            </a>
+            <Form action="/logout" method="post" className="nav-link px-3">
+              <button
+                className="btn btn-dark d-flex align-items-center gap-1"
+                id="logout-btn"
+                type="submit"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  className="feather"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
+                  />
+                </svg>
+                Sign out
+              </button>
+            </Form>
           </div>
         </div>
       </header>
@@ -102,32 +123,7 @@ export default function DashboardIndexRoute() {
                     {user.email}
                   </i>
                 </li>
-                <li className="nav-item">
-                  <Form action="/logout" method="post" className="nav-link">
-                    <button
-                      className="btn btn-sm btn-outline-secondary d-flex align-items-center"
-                      id="logout-btn"
-                      type="submit"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        className="feather"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"
-                        />
-                        <path
-                          fillRule="evenodd"
-                          d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
-                        />
-                      </svg>
-                      Logout
-                    </button>
-                  </Form>
-                </li>
+
                 <li className="nav-item">
                   <Form
                     action="/dashboard/delete-user"
