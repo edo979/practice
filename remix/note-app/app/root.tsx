9e2d7d1b15c1ef854fe3ea1cf26e1403515beb01
edo979 <prezentabilan@gmail.com>
2022-12-25 16:52:43 +0100

    basic layout dashboard

--- remix/note-app/app/root.tsx
+++ remix/note-app/app/root.tsx
@@ -13,17 +13,9 @@ export const links: LinksFunction = () => {
     {
       as: 'style',
       rel: 'stylesheet preload prefetch',
-      href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css',
+      href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
       integrity:
-        'sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65',
-      crossOrigin: 'anonymous',
-    },
-    {
-      as: 'script',
-      rel: 'modulepreload',
-      href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js',
-      integrity:
-        'sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V',
+        'sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD',
       crossOrigin: 'anonymous',
     },
   ]
@@ -44,11 +36,11 @@ export default function App() {
       </head>
       <body>
         <Outlet />
-        {/* <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-          integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
           crossOrigin="anonymous"
-        ></script> */}
+        ></script>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
