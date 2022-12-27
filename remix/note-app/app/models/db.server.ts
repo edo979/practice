404d98bc4866b7fa148fc21f0bc7104fbc278a41
edo979 <prezentabilan@gmail.com>
2022-12-27 10:04:03 +0100

    fix connection to db

--- remix/note-app/app/models/db.server.ts
+++ remix/note-app/app/models/db.server.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI!)
+  await mongoose.connect(process.env.MONGODB_URL!)
   console.log('Mongo DB connected')
 }
 
