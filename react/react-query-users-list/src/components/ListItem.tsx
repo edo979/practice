20f8b3c776c5685c3b6f6c7126ac829ccf93e01d
edo979 <prezentabilan@gmail.com>
2023-01-05 08:09:47 +0100

    refactor list item comp

--- react/react-query-users-list/src/components/ListItem.tsx
+++ react/react-query-users-list/src/components/ListItem.tsx
@@ -7,22 +7,44 @@ type ListItemProps = {
 
 export default function ListItem({ user }: ListItemProps) {
   const [isEditing, setIsEditing] = useState(false)
+  const [userName, setUserName] = useState(user.name)
+
+  async function updateUser() {}
 
   return (
     <li className="user-list_item" key={user.id}>
-      {isEditing ? <input type="text" /> : <p className="m-0">{user.name}</p>}
-      <span className="controll-icons">
-        <button className="btn btn-icon" onClick={() => setIsEditing(true)}>
-          <svg className="bi">
-            <use xlinkHref="#icon-edit" />
-          </svg>
-        </button>
-        <button className="btn btn-icon">
-          <svg className="bi">
-            <use xlinkHref="#icon-delete" />
-          </svg>
-        </button>
-      </span>
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <button className="btn btn-icon" onClick={updateUser}>
+            <svg className="bi">
+              <use xlinkHref="#icon-check" />
+            </svg>
+          </button>
+        </>
+      ) : (
+        <>
+          <p className="m-0">{user.name}</p>
+
+          <span className="controll-icons">
+            <button className="btn btn-icon" onClick={() => setIsEditing(true)}>
+              <svg className="bi">
+                <use xlinkHref="#icon-edit" />
+              </svg>
+            </button>
+
+            <button className="btn btn-icon">
+              <svg className="bi">
+                <use xlinkHref="#icon-delete" />
+              </svg>
+            </button>
+          </span>
+        </>
+      )}
     </li>
   )
 }
