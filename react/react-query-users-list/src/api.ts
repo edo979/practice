51e90fe5ec959a10a836ba8a6d76f796e66a0136
edo979 <prezentabilan@gmail.com>
2023-01-06 07:45:09 +0100

    add uuid for user id

--- react/react-query-users-list/src/api.ts
+++ react/react-query-users-list/src/api.ts
@@ -1,20 +1,11 @@
+import { v4 as uuid4 } from 'uuid'
+
 export type User = {
   id: number
   name: string
 }
 
 const API_URL = 'http://localhost:5000'
-
-export async function getId() {
-  const res = await fetch(`${API_URL}/users?_page=1&_limit=1`)
-  const count = res.headers.get('x-total-count')
-
-  if (!count) {
-    throw new Error("Cant't get id")
-  }
-
-  return parseInt(count) + 1
-}
 
 export async function getUsers() {
   await new Promise((resolve) => setTimeout(resolve, 500))
@@ -28,13 +19,12 @@ export async function getUsers() {
 export async function createUser(name: string) {
   await new Promise((resolve) => setTimeout(resolve, 500))
 
-  const id = await getId()
   const res = await fetch(`${API_URL}/users`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ id, name }),
+    body: JSON.stringify({ id: uuid4(), name }),
   })
 
   return await res.json()
