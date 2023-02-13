82f908e42453d62549b4f39793033dee7cee4288
edo979 <prezentabilan@gmail.com>
2023-02-13 11:38:04 +0100

    successfuly send request from server

--- staticSites/weatherApp/api/index.js
+++ staticSites/weatherApp/api/index.js
@@ -1,5 +1,19 @@
-import { data } from '../src/data'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-export default function handler(request, response) {
-  response.status(200).json(data)
+export default async function handler(request, response) {
+  try {
+    const res = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?lat=43.6685&lon=18.9749&appid=${process.env.API_KEY}&units=metric`
+    )
+
+    if (res.ok) {
+      const data = await res.json()
+      return response.status(200).json(data)
+    }
+
+    return response.status(400).end()
+  } catch {
+    return response.status(500).end()
+  }
 }
