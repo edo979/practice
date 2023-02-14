edc9e93e0ab4b5915c22bc81ec425b4433c433b3
edo979 <prezentabilan@gmail.com>
2023-02-14 21:34:29 +0100

    add button for search city

--- staticSites/weatherApp/src/App.tsx
+++ staticSites/weatherApp/src/App.tsx
@@ -18,9 +18,22 @@ function App() {
         <div className="row-start-1 mx-auto grid min-h-screen max-w-2xl grid-rows-[auto_minmax(200px,_max-content)_auto_auto_1fr] py-8 px-4">
           <div className="row-start-1 flex items-end justify-between">
             <div className="flex flex-col">
-              <h1 className="text-2xl uppercase text-white sm:text-3xl">
-                {data?.city}
-              </h1>
+              <div className="flex min-w-[180px] flex-row items-center justify-between gap-2">
+                <h1 className="text-2xl uppercase text-white sm:text-3xl">
+                  {data?.city}
+                </h1>
+                <button className="z-10 cursor-pointer rounded-md border border-blue-50 border-opacity-30 bg-blue-50 bg-opacity-20 py-1.5 px-2 text-white hover:border-opacity-40 hover:bg-opacity-30">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                  </svg>
+                </button>
+              </div>
               <p className="font-light text-white opacity-80 sm:text-lg">
                 {new Date().toLocaleDateString('sr-Latn', {
                   month: 'long',
