82bbefe04571c4253c5b55551db8c60456feebdb
edo979 <prezentabilan@gmail.com>
2023-02-18 18:32:20 +0100

    add react components

---
+++ astro/countries/src/components/Countries.tsx
@@ -0,0 +1,11 @@
+import CountriesList from './CountriesList'
+import SeacrhBar from './SeacrhBar'
+
+export default function Countries() {
+  return (
+    <main>
+      <SeacrhBar />
+      <CountriesList />
+    </main>
+  )
+}
