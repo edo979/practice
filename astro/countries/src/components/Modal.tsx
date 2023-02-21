0115daafc40f3f9b2db5456c72eccf56d2ba0248
edo979 <prezentabilan@gmail.com>
2023-02-21 11:54:26 +0100

    finish mobile view

--- astro/countries/src/components/Modal.tsx
+++ astro/countries/src/components/Modal.tsx
@@ -21,7 +21,7 @@ export default function Modal({
 
   return (
     <div className="fixed top-0 left-0 h-full w-full p-6 grid grid-cols-[minmax(320px,_576px)] place-content-center bg-black/70">
-      <div className="p-4 border border-stone-600 rounded bg-stone-800">
+      <div className="p-4 border border-stone-600 rounded bg-stone-800 overflow-y-auto">
         <button
           onClick={setHidden}
           className="block py-1 px-3 ml-auto rounded-md border border-stone-500 bg-stone-600 text-stone-300 font-light"
@@ -56,23 +56,35 @@ export default function Modal({
                 {country.population.toLocaleString('hr-BA')}
               </p>
             </div>
-
-            <div>
-              <p className="text-xs text-stone-500 tracking-tighter uppercase font-semibold">
-                Kontinet:
-              </p>
-              <p className="text-lg">{regions[`${country.region}`]}</p>
-            </div>
           </div>
 
           <div className="space-y-2">
             <div>
               <p className="text-xs text-stone-500 tracking-tighter uppercase font-semibold">
-                Zvanični jezici:
+                Susjedne države:
               </p>
-              <p className="text-lg">
-                {Object.keys(country.languages).join(' ,')}
+              <div className="mt-2 flex flex-row flex-wrap gap-2">
+                {country.borders ? (
+                  country.borders.map((cca3) => (
+                    <button
+                      key={cca3}
+                      className="btn btn-primary"
+                      onClick={() => getCountry(cca3)}
+                    >
+                      {getCountryName(cca3)}
+                    </button>
+                  ))
+                ) : (
+                  <p>Nema kopnenih susjednih država.</p>
+                )}
+              </div>
+            </div>
+
+            <div className="pt-4">
+              <p className="text-xs text-stone-500 tracking-tighter uppercase font-semibold">
+                Kontinet:
               </p>
+              <p className="text-lg">{regions[`${country.region}`]}</p>
             </div>
 
             <div>
@@ -84,21 +96,22 @@ export default function Modal({
               </p>
             </div>
 
-            <p>Susjedne države:</p>
-            <div className="flex flex-row flex-wrap gap-2">
-              {country.borders ? (
-                country.borders.map((cca3) => (
-                  <button
-                    key={cca3}
-                    className="btn btn-primary"
-                    onClick={() => getCountry(cca3)}
-                  >
-                    {getCountryName(cca3)}
-                  </button>
-                ))
-              ) : (
-                <p>Nema kopnenih susjednih država.</p>
-              )}
+            <div>
+              <p className="text-xs text-stone-500 tracking-tighter uppercase font-semibold">
+                Zvanični jezici:
+              </p>
+              <p className="text-lg">
+                {Object.keys(country.languages).join(' ,')}
+              </p>
+            </div>
+
+            <div>
+              <p className="text-xs text-stone-500 tracking-tighter uppercase font-semibold">
+                Skraćenice za državu:
+              </p>
+              <p className="text-lg">
+                <span>{country.cca2}</span>, <span>{country.cca3}</span>
+              </p>
             </div>
           </div>
         </div>
