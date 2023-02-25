458a5da14eff8b3aa3f661b326923a2093e0fee3
edo979 <prezentabilan@gmail.com>
2023-02-25 13:49:04 +0100

    add opneai send first request

--- staticSites/imageGenerator/api/index.js
+++ staticSites/imageGenerator/api/index.js
@@ -1,4 +1,15 @@
-export default function handler(req, res) {
-  const { name = 'World' } = req.query
-  return res.json({ message: `Hello ${name}!` })
+import { Configuration, OpenAIApi } from 'openai'
+
+export default async function handler(req, res) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+  const openai = new OpenAIApi(configuration)
+  const response = await openai.createImage({
+    prompt: 'electrons inside the computer in futuristic style',
+    n: 1,
+    size: '256x256',
+  })
+
+  return res.json({ image_url: response.data.data[0].url })
 }
