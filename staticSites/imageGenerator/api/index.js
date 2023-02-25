afe6ee49fb5d212ff8be30e1d7d11a7cb442d662
edo979 <prezentabilan@gmail.com>
2023-02-25 19:58:57 +0100

    add error message section

--- staticSites/imageGenerator/api/index.js
+++ staticSites/imageGenerator/api/index.js
@@ -11,15 +11,20 @@ export default async function handler(req, res) {
   if (typeof prompt !== 'string' || prompt.trim() === '')
     return res.status(400).end()
 
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  })
-  const openai = new OpenAIApi(configuration)
-  const response = await openai.createImage({
-    prompt,
-    n: 1,
-    size: '256x256',
-  })
+  try {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    })
+    const openai = new OpenAIApi(configuration)
 
-  return res.json({ image_url: response.data.data[0].url })
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '256x256',
+    })
+
+    return res.json({ image_url: response.data.data[0].url })
+  } catch (error) {
+    return res.status(500).end()
+  }
 }
