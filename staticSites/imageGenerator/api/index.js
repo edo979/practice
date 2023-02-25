2489c6a269e6e4f0ba8bf73cdcb0dd21a676982d
edo979 <prezentabilan@gmail.com>
2023-02-25 14:16:12 +0100

    link form and api on the server

--- staticSites/imageGenerator/api/index.js
+++ staticSites/imageGenerator/api/index.js
@@ -1,12 +1,22 @@
 import { Configuration, OpenAIApi } from 'openai'
 
 export default async function handler(req, res) {
+  if (
+    req.method !== 'POST' ||
+    req.headers['content-type'] !== 'application/json'
+  )
+    return res.status(400).end()
+
+  const { prompt } = req.body
+  if (typeof prompt !== 'string' || prompt.trim() === '')
+    return res.status(400).end()
+
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   })
   const openai = new OpenAIApi(configuration)
   const response = await openai.createImage({
-    prompt: 'electrons inside the computer in futuristic style',
+    prompt,
     n: 1,
     size: '256x256',
   })
