83d44436d1eb6a4c849efb7c341ada36449b38e1
edo979 <prezentabilan@gmail.com>
2023-03-02 13:49:14 +0100

    do some practice

---
+++ quickPractice/colors-astro/src/data/colors.ts
@@ -0,0 +1,9 @@
+export const colors = [
+  { name: 'Lavender', value: '#9b5fe0' },
+  { name: 'Blue', value: '#16a4d8' },
+  { name: 'Crayola', value: '#60dbe8' },
+  { name: 'Kiwi', value: '#8bd346' },
+  { name: 'Yellow', value: '#efdf48' },
+  { name: 'Saffron', value: '#f9a52c' },
+  { name: 'Sinopia', value: '#d64e12' },
+]
