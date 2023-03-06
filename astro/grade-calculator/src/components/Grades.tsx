188f33e748af0ee2d1c9010e47df15fbec985261
edo979 <prezentabilan@gmail.com>
2023-03-06 12:24:06 +0100

    basic layout, handle grade change

--- astro/grade-calculator/src/components/Grades.tsx
+++ astro/grade-calculator/src/components/Grades.tsx
@@ -1,3 +1,54 @@
+import { useMemo, useState } from 'react'
+import { getClassNumberFromLS, getSubjects } from '../data/util'
+
 export default function Grades() {
-  return <div>Grades</div>
+  const subjects = useMemo(() => {
+    console.log('call LS')
+    return getSubjects(getClassNumberFromLS())
+  }, [])
+
+  const [state, setState] = useState({ currentGrade: 5 })
+
+  function handleGrade(grade: number) {
+    setState((prev) => ({ ...prev, currentGrade: grade }))
+  }
+
+  console.log('render')
+
+  return (
+    <div>
+      <p>Ime i prezime učenika</p>
+      <p>Predmet:</p>
+      <p className="text-xl">Bosanski</p>
+      <label htmlFor="grade">Ocjena:</label>
+      <input
+        type="number"
+        name="grade"
+        id="grade"
+        min={1}
+        max={5}
+        className="border block"
+        value={state.currentGrade}
+        disabled
+      />
+
+      <div className="flex gap-1">
+        <button className="btn" onClick={() => handleGrade(1)}>
+          1
+        </button>
+        <button className="btn" onClick={() => handleGrade(2)}>
+          2
+        </button>
+        <button className="btn" onClick={() => handleGrade(3)}>
+          3
+        </button>
+        <button className="btn" onClick={() => handleGrade(4)}>
+          4
+        </button>
+        <button className="btn" onClick={() => handleGrade(5)}>
+          5
+        </button>
+      </div>
+    </div>
+  )
 }
