4bb4c873fc20bd4ee1d5017863f05d787acb8042
edo979 <prezentabilan@gmail.com>
2023-03-15 12:15:28 +0100

    add scroll to table in results

--- astro/grade-calculator/src/components/Results.tsx
+++ astro/grade-calculator/src/components/Results.tsx
@@ -16,10 +16,14 @@ export default function Results() {
   const students = getStudentsFromLS()
 
   return (
-    <div className="max-w-lg px-2 print-page">
-      <section className="print print__landscape">
-        <p>Ocjene učenika:</p>
-        <table className="table-fixed border-collapse border border-slate-400 text-sm">
+    <div className="px-2 print-page">
+      <h1 className="mt-4 text-2xl font-bold md:text-5xl md:mt-8">
+        Rezultati razreda
+      </h1>
+
+      <section className="print print__landscape overflow-x-scroll">
+        <h2 className="mt-8 text-xl font-bold md:text-3xl">Ocjene učenika:</h2>
+        <table className="mt-2 table-fixed border-collapse border border-slate-400 text-sm">
           <thead>
             <tr>
               <th className="border border-slate-400">Br.</th>
@@ -65,74 +69,85 @@ export default function Results() {
         </table>
       </section>
 
-      <section className="print">
-        <p>Ocjene razreda:</p>
-        <table className="table-auto border-collapse border border-slate-400 text-sm">
-          <thead>
-            <tr>
-              <th className="border border-slate-400 px-1">5</th>
-              <th className="border border-slate-400 px-1">4</th>
-              <th className="border border-slate-400 px-1">3</th>
-              <th className="border border-slate-400 px-1">2</th>
-              <th className="border border-slate-400 px-1">Ukupno</th>
-              <th className="border border-slate-400 px-1">1.slaba</th>
-              <th className="border border-slate-400 px-1">2.slabe</th>
-              <th className="border border-slate-400 px-1">3.slabe</th>
-              <th className="border border-slate-400 px-1">Ukupno</th>
-              <th className="border border-slate-400 px-1">Pr.Razreda</th>
-              <th className="border border-slate-400 px-1">Pr.Ocjena</th>
-            </tr>
-          </thead>
-          <tbody>
-            {getClassResults(students).map((row, i) => (
-              <tr key={i} className="odd:bg-slate-200">
-                {row.map((result, i) => (
-                  <td
-                    key={i}
-                    className="px-1 border border-slate-400 text-center"
-                  >
-                    {result}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <section className="mt-4 print">
+        <h2 className="text-xl font-bold md:text-3xl">Ocjene razreda:</h2>
 
-        <p>Ocjene predmeta:</p>
-        <table className="table-auto border-collapse border border-slate-400 text-sm">
-          <thead>
-            <tr>
-              <th className="border border-slate-400 px-1">Predmet</th>
-              <th className="border border-slate-400 px-1">5</th>
-              <th className="border border-slate-400 px-1">4</th>
-              <th className="border border-slate-400 px-1">3</th>
-              <th className="border border-slate-400 px-1">2</th>
-              <th className="border border-slate-400 px-1">1</th>
-              <th className="border border-slate-400 px-1">Prolazi</th>
-              <th className="border border-slate-400 px-1">Prosjek</th>
-            </tr>
-          </thead>
-          <tbody>
-            {getSubjectsResult(students, subjects).map((subjectResults, i) => (
-              <tr key={i} className="odd:bg-slate-200">
-                {subjectResults.map((result, i) => (
-                  <td
-                    key={i}
-                    className="px-1 border border-slate-400 text-center"
-                  >
-                    {result}
-                  </td>
-                ))}
+        <div className="overflow-x-scroll">
+          <table className="w-max mt-2 table-auto border-collapse border border-slate-400 text-sm">
+            <thead>
+              <tr>
+                <th className="border border-slate-400 px-1">5</th>
+                <th className="border border-slate-400 px-1">4</th>
+                <th className="border border-slate-400 px-1">3</th>
+                <th className="border border-slate-400 px-1">2</th>
+                <th className="border border-slate-400 px-1">Ukupno</th>
+                <th className="border border-slate-400 px-1">1.slaba</th>
+                <th className="border border-slate-400 px-1">2.slabe</th>
+                <th className="border border-slate-400 px-1">3.slabe</th>
+                <th className="border border-slate-400 px-1">Ukupno</th>
+                <th className="border border-slate-400 px-1">Pr.Razreda</th>
+                <th className="border border-slate-400 px-1">Pr.Ocjena</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {getClassResults(students).map((row, i) => (
+                <tr key={i} className="odd:bg-slate-200">
+                  {row.map((result, i) => (
+                    <td
+                      key={i}
+                      className="px-1 border border-slate-400 text-center"
+                    >
+                      {result}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className="mt-4 overflow-x-scroll">
+          <h2 className="text-xl font-bold md:text-3xl">Ocjene predmeta:</h2>
+          <table className="w-max mt-2 table-auto border-collapse border border-slate-400 text-sm">
+            <thead>
+              <tr>
+                <th className="border border-slate-400 px-1">Predmet</th>
+                <th className="border border-slate-400 px-1">5</th>
+                <th className="border border-slate-400 px-1">4</th>
+                <th className="border border-slate-400 px-1">3</th>
+                <th className="border border-slate-400 px-1">2</th>
+                <th className="border border-slate-400 px-1">1</th>
+                <th className="border border-slate-400 px-1">Prolazi</th>
+                <th className="border border-slate-400 px-1">Prosjek</th>
+              </tr>
+            </thead>
+            <tbody>
+              {getSubjectsResult(students, subjects).map(
+                (subjectResults, i) => (
+                  <tr key={i} className="odd:bg-slate-200">
+                    {subjectResults.map((result, i) => (
+                      <td
+                        key={i}
+                        className="px-1 border border-slate-400 text-center"
+                      >
+                        {result}
+                      </td>
+                    ))}
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
+        </div>
       </section>
 
-      <button className="btn" onClick={() => window.print()}>
-        Štampaj
-      </button>
+      <section></section>
+
+      <div className="text-right">
+        <button className="mt-4 ml-auto btn" onClick={() => window.print()}>
+          Štampaj
+        </button>
+      </div>
     </div>
   )
 }
