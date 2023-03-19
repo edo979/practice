88e4ae8ae4d711bbc403020d03438bcb2e6e557a
edo979 <prezentabilan@gmail.com>
2023-03-19 23:26:30 +0100

    add more buttons and bg-triangle

--- react/paperRockScissors/src/App.tsx
+++ react/paperRockScissors/src/App.tsx
@@ -7,8 +7,24 @@ function App() {
         <h1 className=" text-3xl text-white font-bold">Hello World</h1>
       </section>
 
-      <section className="mt-8 py-8 border">
-        <GameButton buttonType="rock" />
+      <section className="mt-8 h-72 w-72 mx-auto relative">
+        <div className="absolute top-0 left-0 isolate">
+          <GameButton buttonType="paper" />
+        </div>
+
+        <div className="absolute top-0 right-0 isolate">
+          <GameButton buttonType="scissors" />
+        </div>
+
+        <div className="absolute bottom-0 left-20 isolate">
+          <GameButton buttonType="rock" />
+        </div>
+
+        <div className="w-full h-full flex items-center justify-center">
+          <svg className="w-[70%]" viewBox="0 0 313 278">
+            <use xlinkHref="#triangle"></use>
+          </svg>
+        </div>
       </section>
     </div>
   )
--- react/paperRockScissors/src/components/GameButton.tsx
+++ react/paperRockScissors/src/components/GameButton.tsx
@@ -3,29 +3,20 @@ type GameButtonProps = {
 }
 
 export default function GameButton({ buttonType }: GameButtonProps) {
-  let buttonShadow
-
-  switch (buttonType) {
-    case 'scissors':
-      buttonShadow = 'yellow-600'
-      break
-    case 'paper':
-      buttonShadow = 'blue-600'
-      break
-    case 'rock':
-      buttonShadow = 'rose-600'
-      break
-
-    default:
-      buttonShadow = 'yellow-600'
-  }
   return (
     <button
       className={`w-32 relative aspect-square flex items-center justify-center rounded-full bg-${buttonType}`}
     >
       <div
-        className={`w-full top-1.5 left-0 -z-10 absolute aspect-square rounded-full bg-${buttonShadow}`}
+        className={`w-full top-1.5 left-0 absolute -z-20 aspect-square rounded-full ${
+          buttonType === 'scissors'
+            ? 'bg-yellow-700'
+            : buttonType === 'paper'
+            ? 'bg-blue-700'
+            : 'bg-rose-700'
+        }`}
       ></div>
+
       {/* gray shadow on top */}
       <div className="w-24 aspect-square relative flex items-center justify-center rounded-full overflow-hidden bg-gray-300">
         {/* white background */}
