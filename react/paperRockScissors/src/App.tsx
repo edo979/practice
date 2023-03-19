7bdb3b911235669022f6e6565821f75841b57fd5
edo979 <prezentabilan@gmail.com>
2023-03-19 09:21:02 +0100

    add color

--- react/paperRockScissors/src/App.tsx
+++ react/paperRockScissors/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="container"></div>
+  return (
+    <div className="container">
+      <h1 className="text-3xl text-white font-bold">Hello World</h1>
+    </div>
+  )
 }
 
 export default App
