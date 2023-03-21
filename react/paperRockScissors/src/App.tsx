7a81d432d82d68c52459ae66a1becaf3f1c49352
edo979 <prezentabilan@gmail.com>
2023-03-21 23:56:51 +0100

    add hover state, add to style

--- react/paperRockScissors/src/App.tsx
+++ react/paperRockScissors/src/App.tsx
@@ -27,7 +27,7 @@ function App() {
 
         <section className="mt-6 flex justify-center ">
           <button
-            className="uppercase py-2 px-4 rounded-md border-2 border-neutralBlue50 bg-transparent text-neutralBlue50"
+            className="uppercase py-2 px-4 rounded-md border-2 border-neutralBlue50 bg-transparent text-neutralBlue50 hover:border-slate-50 hover:text-slate-50"
             onClick={() => setShowModal(true)}
           >
             Pravila
@@ -37,10 +37,14 @@ function App() {
 
       {showModal && (
         <section className="absolute inset-0 p-8 flex items-center justify-center bg-slate-900/80">
-          <div className="w-full p-4 pb-8 rounded-md bg-slate-50">
+          <div className="w-full p-4 pb-8 max-w-xs rounded-md bg-slate-50">
             <div className="mr-2 mt-2 mb-8 flex justify-end">
               <button onClick={() => setShowModal(false)}>
-                <svg width="20" height="20">
+                <svg
+                  width="20"
+                  height="20"
+                  className="fill-stone-400 hover:fill-rose-600"
+                >
                   <use xlinkHref="#close-btn"></use>
                 </svg>
               </button>
