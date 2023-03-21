9dadcc57acbc5ed3520cfff0475a83994e24016f
edo979 <prezentabilan@gmail.com>
2023-03-21 10:41:08 +0100

    add placeholder and text

--- react/paperRockScissors/src/components/Compare.tsx
+++ react/paperRockScissors/src/components/Compare.tsx
@@ -10,15 +10,21 @@ export default function Compare() {
 
   return (
     <div className="relative w-72 mx-auto mt-8">
-      <div className="absolute left-0 top-0">
-        <Button buttonType={userPick} />
-      </div>
-
-      {housePick && (
-        <div className="absolute right-0 top-0">
-          <Button buttonType={housePick} />
+      <div className="flex justify-between gap-8">
+        <div className="basis-1/2 flex flex-col items-center">
+          <Button buttonType={userPick} />
+          <p className="mt-4 text-slate-50 uppercase">Vaš izbor</p>
         </div>
-      )}
+
+        <div className="basis-1/2 flex flex-col items-center justify-between">
+          {housePick ? (
+            <Button buttonType={housePick} />
+          ) : (
+            <div className="w-24 h-24 my-auto rounded-full bg-slate-900"></div>
+          )}
+          <p className="mt-4 text-slate-50 uppercase">Izbor Aplikacije</p>
+        </div>
+      </div>
     </div>
   )
 }
