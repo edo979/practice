d55a375d507877c1a6ea746a6622fcc4022648be
edo979 <prezentabilan@gmail.com>
2023-05-04 23:34:05 +0200

    add db

--- reactNative/FavoriteLocationApp/App.js
+++ reactNative/FavoriteLocationApp/App.js
@@ -1,19 +1,38 @@
+import { useCallback, useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
+import * as SplashScreen from 'expo-splash-screen'
 import AllPlaces from './screens/AllPlaces'
 import AddPlace from './screens/AddPlace'
 import IconButton from './components/ui/IconButton'
 import { Colors } from './constants/colors'
 import Map from './screens/Map'
+import { init } from './util/database'
+
+SplashScreen.preventAutoHideAsync()
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
+  const [isDBInit, setIsDBinit] = useState(false)
+
+  useEffect(() => {
+    init()
+      .then(() => setIsDBinit(true))
+      .catch((err) => console.log(err))
+  }, [])
+
+  const onReady = useCallback(async () => {
+    if (isDBInit) await SplashScreen.hideAsync()
+  }, [isDBInit])
+
+  if (!isDBInit) return null
+
   return (
     <>
       <StatusBar style="dark" />
-      <NavigationContainer>
+      <NavigationContainer onReady={onReady}>
         <Stack.Navigator
           screenOptions={{
             headerStyle: { backgroundColor: Colors.primary500 },
