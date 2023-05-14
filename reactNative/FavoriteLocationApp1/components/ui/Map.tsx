eb21221984af78df106ccba1440431a8b5c1c8b0
edo979 <prezentabilan@gmail.com>
2023-05-14 08:31:04 +0200

    get user location

--- reactNative/FavoriteLocationApp1/components/ui/Map.tsx
+++ reactNative/FavoriteLocationApp1/components/ui/Map.tsx
@@ -1,6 +1,7 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Alert, PermissionsAndroid, StyleSheet, Text, View} from 'react-native';
 import {main, mainStyle} from '../../constants/style';
 import IconButton from './IconButton';
+import GetLocation from 'react-native-get-location';
 
 type MapProps = {
   location?: {lat: number; lng: number};
@@ -9,6 +10,37 @@ type MapProps = {
 const Map = ({location}: MapProps) => {
   let content = <Text style={{fontSize: main.fsLG}}>Nije izabrano mjesto</Text>;
   if (location) content = <Text>The Map</Text>;
+
+  async function getLocationHandler() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Upozorenje',
+          message: 'Aplikacija želi pristup vašoj tačnoj lokaciji.',
+          buttonPositive: 'OK',
+        },
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        const {latitude, longitude} = await GetLocation.getCurrentPosition({
+          enableHighAccuracy: true,
+          timeout: 6000,
+        });
+
+        console.log(latitude, longitude);
+      } else {
+        Alert.alert(
+          'Upozorenje',
+          'Da bi nastavili koristiti aplikaciju morate dopustiti upotrebu lokacije.',
+        );
+      }
+    } catch (err) {
+      Alert.alert('Upozorenje', 'Došlo je do greške, pokušajte ponovo.');
+    }
+  }
+
+  async function requestLocationPermission() {}
 
   return (
     <View style={{flex: 3, gap: 12}}>
@@ -21,7 +53,7 @@ const Map = ({location}: MapProps) => {
           </IconButton>
         </View>
         <View style={{flex: 1}}>
-          <IconButton name="my-location" onPress={() => {}}>
+          <IconButton name="my-location" onPress={getLocationHandler}>
             Lociraj
           </IconButton>
         </View>
