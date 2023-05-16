0e07338b108a3fd2ac22e76713a36df89b78c19c
edo979 <prezentabilan@gmail.com>
2023-05-16 12:19:15 +0200

    add marker on map

--- reactNative/FavoriteLocationApp1/components/Map.tsx
+++ reactNative/FavoriteLocationApp1/components/Map.tsx
@@ -1,22 +1,34 @@
 import {StyleSheet, Text, View} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import {LocationT} from './GetUserLocation';
+import {useState} from 'react';
 
 type MapPropsT = {
   location?: LocationT;
 };
 
 const Map = ({location}: MapPropsT) => {
+  const [state, setState] = useState<LocationT>({
+    lat: location?.lat ?? 37.78825,
+    lng: location?.lng ?? -122.4324,
+  });
+
   return (
     <MapView
       initialRegion={{
-        latitude: location?.lat ?? 37.78825,
-        longitude: location?.lng ?? -122.4324,
+        latitude: state.lat,
+        longitude: state.lng,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }}
-      style={{height: '100%', width: '100%'}}
-    />
+      style={{height: '100%', width: '100%'}}>
+      <Marker
+        key={1}
+        coordinate={{latitude: state.lat, longitude: state.lng}}
+        title={'Lokacija'}
+        description={'Trenutna lokacija'}
+      />
+    </MapView>
   );
 };
 
