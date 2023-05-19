327fa76d522c415f3b3b03244b216189344d823d
edo979 <prezentabilan@gmail.com>
2023-05-19 10:03:23 +0200

    layout place component

--- reactNative/FavoriteLocationApp1/components/Map.tsx
+++ reactNative/FavoriteLocationApp1/components/Map.tsx
@@ -5,9 +5,10 @@ import {useState} from 'react';
 
 type MapPropsT = {
   location?: LocationT;
+  style?: {};
 };
 
-const Map = ({location}: MapPropsT) => {
+const Map = ({location, style}: MapPropsT) => {
   const [state, setState] = useState<LocationT>({
     lat: location?.lat ?? 37.78825,
     lng: location?.lng ?? -122.4324,
@@ -24,7 +25,7 @@ const Map = ({location}: MapPropsT) => {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }}
-      style={{height: '100%', width: '100%'}}>
+      style={{height: '100%', width: '100%', ...style}}>
       <Marker
         key={1}
         coordinate={{latitude: state.lat, longitude: state.lng}}
--- reactNative/FavoriteLocationApp1/screens/Place.tsx
+++ reactNative/FavoriteLocationApp1/screens/Place.tsx
@@ -1,8 +1,9 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import {PlaceT} from '../store/dt';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {StackParamListT} from '../App';
 import {DarkTheme} from '../constants/style';
+import Map from '../components/Map';
 
 type PlacePropT = NativeStackScreenProps<StackParamListT, 'Place'>;
 
@@ -12,16 +13,39 @@ const Place = ({route}: PlacePropT) => {
   if (!place) return <Text>Nema podataka za mjesto</Text>;
 
   return (
-    <View>
-      <Text style={styles.title}>{place.name}</Text>
+    <View style={styles.container}>
+      <Text style={[styles.text, styles.title]}>{place.name}</Text>
+      <Text style={styles.text}>{place.address}</Text>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{uri: place.imageUri}} />
+        <Map style={styles.map} />
+      </View>
     </View>
   );
 };
 export default Place;
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: DarkTheme.util.padding,
+  },
+  text: {
+    fontSize: DarkTheme.util.fsMD,
+    color: DarkTheme.colors.border,
+  },
   title: {
     fontSize: DarkTheme.util.fsLG,
     fontWeight: 'bold',
-    color: DarkTheme.colors.border,
+  },
+  imageContainer: {
+    flex: 1,
+    gap: 8,
+  },
+  image: {
+    ...DarkTheme.imageBaseStyle,
+    flex: 4,
+  },
+  map: {
+    flex: 3,
   },
 });
