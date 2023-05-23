c1157dce9e9c697e77f2a354bcc84d3fd74ee5b4
edo979 <prezentabilan@gmail.com>
2023-05-23 12:29:00 +0200

    add file for testing purpose

--- astro/blog-v3/src/env.d.ts
+++ astro/blog-v3/src/env.d.ts
@@ -1,1 +1,24 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly FIREBASE_PRIVATE_KEY_ID: string
+  readonly FIREBASE_PRIVATE_KEY: string
+  readonly FIREBASE_PROJECT_ID: string
+  readonly FIREBASE_CLIENT_EMAIL: string
+  readonly FIREBASE_CLIENT_ID: string
+  readonly FIREBASE_AUTH_URI: string
+  readonly FIREBASE_TOKEN_URI: string
+  readonly FIREBASE_AUTH_CERT_URL: string
+  readonly FIREBASE_CLIENT_CERT_URL: string
+  readonly apiKey: string
+  readonly authDomain: string
+  readonly projectId: string
+  readonly storageBucket: string
+  readonly messagingSenderId: string
+  readonly appId: string
+  readonly measurementId: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
