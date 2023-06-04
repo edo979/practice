08fc437ab48f7778b79d87063df401c3ee02231c
edo979 <prezentabilan@gmail.com>
2023-06-04 22:28:21 +0200

    add daisy, react, refactor cart to reacr component

--- astro/my-first-shop/astro.config.mjs
+++ astro/my-first-shop/astro.config.mjs
@@ -1,13 +1,18 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
-import node from '@astrojs/node'
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ config: { applyBaseStyles: false } })],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), react()],
   output: 'server',
   adapter: node({
-    mode: 'standalone',
-  }),
-})
+    mode: 'standalone'
+  })
+});
---
+++ astro/my-first-shop/src/components/Cart.tsx
@@ -0,0 +1,19 @@
+import { useStore } from '@nanostores/react'
+import { $cart } from '../store/store'
+
+const Cart = () => {
+  const cart = useStore($cart)
+
+  return (
+    <div className="relative">
+      <button className="py-0.5 px-1.5 rounded text-2xl hover:bg-slate-100/50">
+        🛒
+      </button>
+      <span className="px-1 absolute top-1 left-0 rounded bg-rose-500 text-xs">
+        {cart.reduce((prev, current) => prev + current.quantity, 0)}
+      </span>
+    </div>
+  )
+}
+
+export default Cart
