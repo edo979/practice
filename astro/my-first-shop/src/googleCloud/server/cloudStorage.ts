34ec783b3fd77077f6c8d315cd251ccbdb223f60
edo979 <prezentabilan@gmail.com>
2023-06-11 23:13:10 +0200

    refactor services, image position

--- astro/my-first-shop/src/googleCloud/server/cloudStorage.ts
+++ astro/my-first-shop/src/googleCloud/server/cloudStorage.ts
@@ -1,7 +1,6 @@
 import { Storage } from '@google-cloud/storage'
 
 const storage = new Storage({
-  projectId: 'my-shop-practice-cd1af',
   keyFilename: './secrets/my-shop-practice-cd1af-storage-admin.json',
 })
 export const bucket = storage.bucket('my-shop-app-storage')
--- astro/my-first-shop/src/googleCloud/server/firestore.ts
+++ astro/my-first-shop/src/googleCloud/server/firestore.ts
@@ -1,6 +1,5 @@
 import { Firestore } from '@google-cloud/firestore'
 
 export const db = new Firestore({
-  projectId: 'my-shop-practice-cd1af',
   keyFilename: './secrets/my-shop-practice-cd1af-80b0e554a9e9.json',
 })
