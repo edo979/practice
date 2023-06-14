e96cad969590ade05b89908b0c1e1a9f51732328
edo979 <prezentabilan@gmail.com>
2023-06-14 23:52:51 +0200

    map data from firestore

--- astro/cable-web-shop/src/firebase/firestore.ts
+++ astro/cable-web-shop/src/firebase/firestore.ts
@@ -2,4 +2,8 @@ import { getFirestore } from 'firebase-admin/firestore'
 
 const productsRef = getFirestore().collection('products')
 
-export const getProducts = async () => (await productsRef.get()).docs
+export const getProducts = async () =>
+  (await productsRef.get()).docs.map((doc) => ({
+    name: doc.data().name,
+    price: doc.data().price,
+  })) as { name: string; price: number }[]
