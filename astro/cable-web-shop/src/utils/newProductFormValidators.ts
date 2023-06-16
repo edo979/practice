5c6ec135bb731c99fbff1274a1a98c65a509aa6d
Edis Selimovic <prezentabilan@gmail.com>
2023-06-16 20:37:42 +0200

    add form validation

---
+++ astro/cable-web-shop/src/utils/newProductFormValidators.ts
@@ -0,0 +1,18 @@
+export const validateProductName = (name: string) => {
+  if (name.length < 3) return 'Name is to short!'
+}
+export const validateProductPrice = (price: string) => {
+  if (Number.isNaN(parseFloat(price)))
+    return 'Please enter valid price in this format 34.44'
+  if (parseFloat(price) <= 0) return 'Add valid price!'
+}
+
+export const validateProductImage = (image: File) => {
+  const allowedFileTypes = ['image/png', 'image/jpeg']
+
+  if (image.size === 0) return 'No image selected.'
+  if (!allowedFileTypes.includes(image.type))
+    return 'Only png, jpg, jpeg images is alloved!'
+
+  if (image.size > 1e6) return 'Max size of image is 1Mb!'
+}
