d44dcee841454b7fe69c016049508be1ef74d206
Edis Selimovic <prezentabilan@gmail.com>
2023-06-17 20:13:35 +0200

    check user when deleting products

--- astro/cable-web-shop/src/pages/api/products/[id].ts
+++ astro/cable-web-shop/src/pages/api/products/[id].ts
@@ -1,11 +1,26 @@
 import type { APIRoute } from 'astro'
+import { getAuth } from 'firebase-admin/auth'
+import { app } from '../../../firebase/server'
 
-export const del: APIRoute = async ({ request, params }) => {
+const auth = getAuth(app)
+
+async function isValidUser(sessionCookie?: string) {
+  if (!sessionCookie) return false
+
+  try {
+    await auth.verifySessionCookie(sessionCookie)
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
+export const del: APIRoute = async ({ request, cookies, params }) => {
   const id = params.id
-  if (!id)
-    return new Response(null, { status: 400, statusText: 'product not found' })
+  if (!id) return new Response(null, { status: 400 })
 
-  return new Response(null, {
-    status: 200,
-  })
+  if (!(await isValidUser(cookies.get('session').value)))
+    return new Response('Not allowed!', { status: 401 })
+
+  return new Response(null, { status: 200 })
 }
