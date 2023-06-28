4adb7f75c3f91a21eb7f0989145a0a8abeb1bae6
Edis Selimovic <prezentabilan@gmail.com>
2023-06-28 19:05:05 +0200

    start editing product

--- astro/cable-web-shop/src/components/dashboard/Product.tsx
+++ astro/cable-web-shop/src/components/dashboard/Product.tsx
@@ -1,6 +1,31 @@
+import { useState } from 'react'
 import type { ProductT } from '../../firebase/utility/firestore'
 
+const ViewProduct = ({ product }: { product: ProductT }) => (
+  <div>
+    <h1>{product.name}</h1>
+    <p>{product.desc}</p>
+    <img src={product.imageUrl} alt={product.name} />
+    <p>
+      <b>{product.price}</b>
+    </p>
+    <p>
+      <i>Added: {product.created_at.toLocaleDateString()}</i>
+    </p>
+  </div>
+)
+
+const EditProduct = ({ cancelEdit }: { cancelEdit: () => void }) => (
+  <div>
+    <button onClick={cancelEdit}>✖️ Cancel</button>
+    <button>💾 Save</button>
+  </div>
+)
+
 const Product = ({ product }: { product: ProductT }) => {
+  const [isEdit, setIsEdit] = useState(false)
+  let content
+
   async function handleDelete() {
     if (!confirm('Pruduct will be deleted. Are you shure?')) return
     const res = await fetch(`/dashboard/${product.id}`, { method: 'delete' })
@@ -8,23 +33,21 @@ const Product = ({ product }: { product: ProductT }) => {
     return
   }
 
-  return (
-    <div>
-      <h1>{product.name}</h1>
-      <p>{product.desc}</p>
-      <img src={product.imageUrl} alt={product.name} />
-      <p>
-        <b>{product.price}</b>
-      </p>
-      <p>
-        <i>Added: {product.created_at.toLocaleDateString()}</i>
-      </p>
+  if (isEdit) {
+    content = <EditProduct cancelEdit={() => setIsEdit(false)} />
+  } else {
+    content = (
+      <>
+        <ViewProduct product={product} />
+        <div>
+          <button onClick={() => setIsEdit(true)}>✏️</button>
+          <button onClick={handleDelete}>❌</button>
+        </div>
+      </>
+    )
+  }
 
-      <div>
-        <button onClick={handleDelete}>Delete</button>
-      </div>
-    </div>
-  )
+  return content
 }
 
 export default Product
