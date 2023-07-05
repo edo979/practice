25df3ad8f0e9e4b6ff2e253f53fca9e233d85670
Edis Selimovic <prezentabilan@gmail.com>
2023-07-05 09:36:32 +0200

    fix quantity of product adding to cart

--- astro/cable-web-shop/src/components/Product.tsx
+++ astro/cable-web-shop/src/components/Product.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import type { ProductT } from '../firebase/utility/firestore'
 import { addToCart } from '../store/cart'
 
@@ -6,12 +7,26 @@ type ProductPropsT = {
 }
 
 const Product = ({ product }: ProductPropsT) => {
+  const [quantity, setQuantity] = useState(1)
+
+  function quantityHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    let quantity = parseInt(e.target.value)
+    if (!quantity || quantity < 1) quantity = 1
+    setQuantity(quantity)
+  }
+
   return (
     <li>
       <h2>{product.name}</h2>
       <p>{product.desc}</p>
       <img src={product.imageUrl} alt={product.name} />
       <p>{product.price}$</p>
+      <input
+        type="number"
+        value={quantity}
+        min="1"
+        onChange={quantityHandler}
+      />
       <button onClick={() => addToCart(product)}>Add to Cart</button>
     </li>
   )
