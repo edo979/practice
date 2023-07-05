042190be6f48da2a51d915d5b374994a4905f841
Edis Selimovic <prezentabilan@gmail.com>
2023-07-05 13:45:43 +0200

    add local storage feature to remember cart between page reload

--- astro/cable-web-shop/src/store/cart.ts
+++ astro/cable-web-shop/src/store/cart.ts
@@ -2,6 +2,22 @@ import { atom } from 'nanostores'
 import type { ProductT } from '../firebase/utility/firestore'
 
 export const $cart = atom<ProductT[]>([])
+
 export const addToCart = (product: ProductT) =>
   $cart.set([...$cart.get(), product])
+
 export const getCartItems = $cart.get()
+
+// Local Storage
+// Update LS on $cart change
+$cart.listen((cart) => localStorage.setItem('cart', JSON.stringify(cart)))
+
+export function getCartFromLS() {
+  const data = localStorage.getItem('cart')
+  if (!data) return null
+  return JSON.parse(data) as ProductT
+}
+
+function clearLS() {
+  localStorage.removeItem('cart')
+}
