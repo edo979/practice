6486f44ad7ecdae043ecd669ab8a6575d5b8afd2
Edis Selimovic <prezentabilan@gmail.com>
2023-07-07 18:05:57 +0200

    style single cart item, add link to product page

--- astro/cable-web-shop/src/components/cart/CartItem.tsx
+++ astro/cable-web-shop/src/components/cart/CartItem.tsx
@@ -3,11 +3,23 @@ import type { CartItemT } from '../../store/cart'
 const CartItem = ({ item }: { item: CartItemT }) => {
   return (
     <li>
-      <a href={`/products/${item.id}`} className="block w-full p-0 m-0">
-        <div className="w-full p-2 rounded-lg bg-base-100 hover:ring-2 hover:ring-accent-focus hover:shadow">
-          <h3 className="text-lg">{item.name}</h3>
-          <img src={item.imageUrl} alt={item.name} />
-          <p className="text-right">
+      <a
+        className="group block rounded-lg bg-base-100 hover:shadow"
+        href={`/products/${item.id}`}
+      >
+        <h3 className="text-lg">
+          <em>{item.quantity}</em> &times; {item.name}
+        </h3>
+        <img
+          src={item.imageUrl}
+          alt={item.name}
+          className="m-0 w-full h-32 object-cover"
+        />
+        <div className="flex">
+          <small className="hidden group-hover:inline-block">
+            Click to update!
+          </small>
+          <p className="ml-auto">
             Price: <strong>{item.price} $</strong>
           </p>
         </div>
