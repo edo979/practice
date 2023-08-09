f3719c9e243fafc491e37f0ede4a868ada3fba42
edo979 <prezentabilan@gmail.com>
2023-08-09 09:00:33 +0200

    add some js

--- pwa/cycleTracker/src/main.ts
+++ pwa/cycleTracker/src/main.ts
@@ -1,1 +1,25 @@
+const newPeriodFormEl = document.getElementsByTagName('form')[0]
+const startDateInputEl = <HTMLInputElement>document.getElementById('start-data')
+const endDateInputEl = <HTMLInputElement>document.getElementById('end-date')
 
+newPeriodFormEl.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const startDate = startDateInputEl.value,
+    endDate = endDateInputEl.value
+
+  if (checkDatesInvalid(startDate, endDate)) return
+
+  storeNewPeriod(startDate, endDate)
+  renderPastPeriods()
+
+  newPeriodFormEl.reset()
+})
+
+function checkDatesInvalid(startDate: string, endDate: string) {
+  return true
+}
+
+function storeNewPeriod(startDate: string, endDate: string) {}
+
+function renderPastPeriods() {}
