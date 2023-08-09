44076a4a912eba86bc959216a652a7e4fd98c1ac
edo979 <prezentabilan@gmail.com>
2023-08-09 09:35:47 +0200

    save input in local storage

--- pwa/cycleTracker/src/main.ts
+++ pwa/cycleTracker/src/main.ts
@@ -1,6 +1,12 @@
 const newPeriodFormEl = document.getElementsByTagName('form')[0]
-const startDateInputEl = <HTMLInputElement>document.getElementById('start-data')
+const startDateInputEl = <HTMLInputElement>document.getElementById('start-date')
 const endDateInputEl = <HTMLInputElement>document.getElementById('end-date')
+const STORAGE_KEY = 'period-tracker-app'
+
+type PeriodT = {
+  startDate: string
+  endDate: string
+}
 
 newPeriodFormEl.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -17,9 +23,28 @@ newPeriodFormEl.addEventListener('submit', (e) => {
 })
 
 function checkDatesInvalid(startDate: string, endDate: string) {
-  return true
+  if (!startDate || !endDate || startDate > endDate) {
+    newPeriodFormEl.reset()
+    return true
+  }
+
+  return false
 }
 
-function storeNewPeriod(startDate: string, endDate: string) {}
+function storeNewPeriod(startDate: string, endDate: string) {
+  const periods = getAllStoredPeriods()
+  periods.push({ startDate, endDate })
+  periods.sort((a, b) => Date.parse(b.startDate) - Date.parse(a.startDate))
+
+  console.log(periods)
+
+  window.localStorage.setItem(STORAGE_KEY, JSON.stringify(periods))
+}
+
+function getAllStoredPeriods() {
+  const data = window.localStorage.getItem(STORAGE_KEY)
+  const periods: PeriodT[] = data ? JSON.parse(data) : []
+  return periods
+}
 
 function renderPastPeriods() {}
