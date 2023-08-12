87b125501706a39f32a282ba8ac73ad0004a3e08
edo979 <prezentabilan@gmail.com>
2023-08-12 22:56:50 +0200

    add sw basic event listeners

---
+++ pwa/learning/public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function (event) {
+  console.log('Service worker: Instaling', event)
+})
+
+self.addEventListener('activate', function (event) {
+  console.log('Service worker: Activating', event)
+  return self.clients.claim()
+})
