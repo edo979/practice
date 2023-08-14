436fdb640074947499f10009cd74ad9b9ffd2cd8
edo979 <prezentabilan@gmail.com>
2023-08-14 08:53:21 +0200

    add files to cache

--- pwa/learning/public/sw.js
+++ pwa/learning/public/sw.js
@@ -3,7 +3,19 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('static').then((cache) => {
       console.log('[Service worker] Precaching App Shell')
-      cache.add('/src/js/app.js')
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+      ])
     })
   )
 })
@@ -14,5 +26,13 @@ self.addEventListener('activate', function (event) {
 })
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(fetch(event.request))
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response
+      } else {
+        return fetch(event.request)
+      }
+    })
+  )
 })
