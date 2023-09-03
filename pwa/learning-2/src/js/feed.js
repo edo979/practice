260864e414ce89ddda19e5b94d2e3b7f9c1b8504
edo979 <prezentabilan@gmail.com>
2023-09-03 10:24:01 +0200

    response main image, add animation

--- pwa/learning-2/src/js/feed.js
+++ pwa/learning-2/src/js/feed.js
@@ -6,7 +6,11 @@ var closeCreatePostModalButton = document.querySelector(
 var sharedMomentsArea = document.querySelector('#shared-moments')
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block'
+  // createPostArea.style.display = 'block'
+  // setTimeout(() => {
+  createPostArea.style.transform = 'translateY(0)'
+  // }, 1)
+
   if (deferredPrompt) {
     deferredPrompt.prompt()
 
@@ -25,7 +29,7 @@ function openCreatePostModal() {
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none'
+  createPostArea.style.transform = 'translateY(100vh)'
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal)
