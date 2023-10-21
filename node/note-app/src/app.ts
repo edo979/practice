e7d073fb375664c2afd7e4fe08f35f185213d6ba
edo979 <prezentabilan@gmail.com>
2023-10-21 22:56:41 +0200

    add first test

--- node/note-app/src/app.ts
+++ node/note-app/src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+app.get('/notes', (req, res) => {
+  res.send('Welcome!')
+})
 
 export default app
---
+++ node/note-app/test/note.test.ts
@@ -0,0 +1,10 @@
+import request from 'supertest'
+import app from '../src/app'
+
+describe('Test crud operations with note', () => {
+  test('Notes page should exist', async () => {
+    const res = await request(app).get('/notes')
+
+    expect(res.statusCode).toEqual(200)
+  })
+})
