a8abf8f18854964e0871dd3650547a01d1ed724f
edo979 <prezentabilan@gmail.com>
2023-10-25 22:26:02 +0200

    test if user data save to db

--- node/note-app/test/user.test.ts
+++ node/note-app/test/user.test.ts
@@ -2,12 +2,14 @@ import request from 'supertest'
 import app from '../src/app'
 import { closeConnection } from '../src/db/mongoose'
 import { setupDatabase } from './fixtures/db'
+import User from '../src/models/user'
+import { response } from 'express'
 
 beforeEach(setupDatabase)
 afterAll(closeConnection)
 
 test('Should create a new user', async () => {
-  await request(app)
+  const res = await request(app)
     .post('/users')
     .send({
       username: 'edi',
@@ -16,4 +18,17 @@ test('Should create a new user', async () => {
       password: 'jahjah',
     })
     .expect(201)
+
+  const user = await User.findById(res.body.user._id)
+
+  expect(user).toBeDefined()
+  expect(res.body).toMatchObject({
+    user: {
+      username: 'edi',
+      email: 'jah@jah.com',
+      age: 44,
+      password: 'jahjah',
+    },
+    token: user?.tokens[0].token,
+  })
 })
