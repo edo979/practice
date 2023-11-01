18f34545e6b6792a4dbdf6d8565f0bc1c2d3dc95
edo979 <prezentabilan@gmail.com>
2023-11-01 07:24:13 +0100

    remove packages

--- node/note-app/src/app.ts
+++ node/note-app/src/app.ts
@@ -1,18 +1,12 @@
 import express from 'express'
-import cookieParser from 'cookie-parser'
-import path from 'path'
 import noteRouter from './routers/noteRouter'
 import userRouter from './routers/userRoutes'
 import './db/mongoose'
 
 const app = express()
-app.set('view engine', 'ejs')
-app.set('views', './src/views')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, '../public')))
-app.use(cookieParser())
 app.use(noteRouter)
 app.use(userRouter)
 
