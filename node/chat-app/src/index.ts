640cc4b26c42d43f0068f91a9d336aea4196271e
edo979 <prezentabilan@gmail.com>
2023-11-02 19:07:43 +0100

    Basic setup of the app

--- node/chat-app/src/index.ts
+++ node/chat-app/src/index.ts
@@ -1,1 +1,12 @@
-console.log('jahh')
+import { createServer } from 'http'
+import path from 'path'
+import express from 'express'
+import { Server } from 'socket.io'
+
+const publicDirPath = path.join(__dirname, '../public')
+
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
+app.use(express.static(publicDirPath))
