c0e5fb85a30414a0fe6d9e7bba4042f5458e8ef0
edo979 <prezentabilan@gmail.com>
2023-12-11 21:28:44 +0100

    style sign in page

--- react/react-router-contacts-practice-app/src/routes/auth/SignIn.jsx
+++ react/react-router-contacts-practice-app/src/routes/auth/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useUserContext } from '../../context/userContext.jsx'
+import '../../assets/css/signin.css'
 
 const SignIn = () => {
   const [email, setEmail] = useState('')
@@ -16,24 +17,49 @@ const SignIn = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
+    <div
+      className="d-flex align-items-center py-4 bg-body-tertiary"
+      style={{ height: '100vh' }}
+    >
+      <main className="form-signin w-100 m-auto">
+        <form onSubmit={handleSubmit}>
+          <div className="d-flex justify-content-between align-items-baseline">
+            <h1 className="h3 mb-3 fw-normal">Please sign in, or...</h1>
+            <Link to="/register">
+              <button className="btn btn-success btn-sm">Register</button>
+            </Link>
+          </div>
+          <div className="form-floating mt-2">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="form-control"
+              placeholder="name@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="email">Email address</label>
+          </div>
+
+          <div className="form-floating">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="form-control"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+          </div>
+
+          <button type="submit" className="btn btn-primary w-100 py-2">
+            Submit
+          </button>
+        </form>
+      </main>
     </div>
   )
 }
