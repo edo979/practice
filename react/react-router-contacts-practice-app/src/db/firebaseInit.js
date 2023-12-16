9f0190deb01dbaffa29b10e7254f2bd3ee82f183
edo979 <prezentabilan@gmail.com>
2023-12-16 20:48:15 +0100

    setup emulator for firestore

--- react/react-router-contacts-practice-app/src/db/firebaseInit.js
+++ react/react-router-contacts-practice-app/src/db/firebaseInit.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore/lite'
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore/lite'
 import { getAuth, connectAuthEmulator } from 'firebase/auth'
 import { firebaseConfig } from '../../secret'
 
@@ -8,3 +8,4 @@ const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
 export const auth = getAuth(app)
 connectAuthEmulator(auth, 'http://127.0.0.1:9099')
+connectFirestoreEmulator(db, '127.0.0.1', 8080)
