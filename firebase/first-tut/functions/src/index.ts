d5066cad6c60b79f57c07052c17c3db53046b628
edo979 <prezentabilan@gmail.com>
2023-12-24 15:59:51 +0100

    add new function

--- firebase/first-tut/functions/src/index.ts
+++ firebase/first-tut/functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as v2 from 'firebase-functions/v2'
+import * as v1 from 'firebase-functions/v1'
 
 type Indexable = { [key: string]: any }
 
@@ -10,3 +11,14 @@ export const helloworld = v2.https.onRequest((request, response) => {
 
   response.send(`<h1>${message}</h1>`)
 })
+
+type Sku = { name: string; usd: number; eur?: number }
+const USD_TO_EUROS = 0.95
+
+export const newsku = v1.firestore
+  .document('/inventory/{sky}')
+  .onCreate((snapshot) => {
+    const data = snapshot.data() as Sku
+    const eur = data.usd * USD_TO_EUROS
+    return snapshot.ref.update({ eur })
+  })
