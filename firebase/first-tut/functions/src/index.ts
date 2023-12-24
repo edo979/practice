1c187410721593e3942d5021ff569a9c2e360acd
edo979 <prezentabilan@gmail.com>
2023-12-24 20:47:44 +0100

    write function to write into firesotre

--- firebase/first-tut/functions/src/index.ts
+++ firebase/first-tut/functions/src/index.ts
@@ -1,24 +1,48 @@
-import * as v2 from 'firebase-functions/v2'
-import * as v1 from 'firebase-functions/v1'
+//import { logger } from 'firebase-functions'
+import { onRequest } from 'firebase-functions/v2/https'
+//import { onDocumentCreated } from 'firebase-functions/v2/firestore'
 
-type Indexable = { [key: string]: any }
+import { initializeApp } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
 
-export const helloworld = v2.https.onRequest((request, response) => {
-  debugger
-  const name = request.params[0]
-  const items: Indexable = { lamp: 'This is a lamp', chair: 'Good chair' }
-  const message = items[name]
+initializeApp()
 
-  response.send(`<h1>${message}</h1>`)
+// import * as v2 from 'firebase-functions/v2'
+// import * as v1 from 'firebase-functions/v1'
+
+//type Indexable = { [key: string]: any }
+
+// export const helloworld = v2.https.onRequest((request, response) => {
+//   debugger
+//   const name = request.params[0]
+//   const items: Indexable = { lamp: 'This is a lamp', chair: 'Good chair' }
+//   const message = items[name]
+
+//   response.send(`<h1>${message}</h1>`)
+// })
+
+//type Sku = { name: string; usd: number; eur?: number }
+//const USD_TO_EUROS = 0.95
+
+// export const newsku = v1.firestore
+//   .document('/inventory/{sku}')
+//   .onCreate((snapshot) => {
+//     const data = snapshot.data() as Sku
+//     const eur = data.usd * USD_TO_EUROS
+//     return snapshot.ref.set({ eur, ...data }, { merge: true })
+//   })
+
+// export const newsku = onDocumentCreated('/inventory/{sky}', (event) => {
+//   const eur = event.data?.data().usd * USD_TO_EUROS
+//   return event.data?.ref.set({ eur }, { merge: true })
+// })
+
+export const newsku = onRequest(async (req, res) => {
+  const usdFromQuery = req.query.usd as string
+  const usd = parseFloat(usdFromQuery)
+
+  const writeResult = await getFirestore()
+    .collection('inventory')
+    .add({ ...req.query, eur: usd * 0.5 })
+  res.json({ result: writeResult.id })
 })
-
-type Sku = { name: string; usd: number; eur?: number }
-const USD_TO_EUROS = 0.95
-
-export const newsku = v1.firestore
-  .document('/inventory/{sky}')
-  .onCreate((snapshot) => {
-    const data = snapshot.data() as Sku
-    const eur = data.usd * USD_TO_EUROS
-    return snapshot.ref.update({ eur })
-  })
