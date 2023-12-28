6b5cf1b7ba9a67fc86ca7181f40437e91a18505f
edo979 <prezentabilan@gmail.com>
2023-12-28 19:28:25 +0100

    add react router dom and root route

--- react/react-router-shop/frontend/src/main.jsx
+++ react/react-router-shop/frontend/src/main.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './db/init.js'
 import App from './App.jsx'
-import './index.css'
+// Router
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './routes/root.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
