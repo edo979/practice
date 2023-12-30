53b79d175d24abefb2c2e58f572a9ad62e2f6b3f
edo979 <prezentabilan@gmail.com>
2023-12-30 11:29:35 +0100

    add footer

--- react/react-router-shop/frontend/src/layouts/MainLayout.jsx
+++ react/react-router-shop/frontend/src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const MainLayout = () => {
   return (
@@ -10,6 +11,7 @@ const MainLayout = () => {
       <main className="container py-5">
         <Outlet />
       </main>
+      <Footer />
     </>
   )
 }
