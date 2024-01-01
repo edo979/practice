6954e53b666046d5558d4fd0cb2bd8b7e076f84b
edo979 <prezentabilan@gmail.com>
2024-01-01 14:29:29 +0100

    add storage to project

--- react/react-router-shop/frontend/src/db/init.js
+++ react/react-router-shop/frontend/src/db/init.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { connectFunctionsEmulator, getFunctions } from 'firebase/functions'
 import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore'
+import { connectStorageEmulator, getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   projectId: 'demo-app',
@@ -12,3 +13,8 @@ export const functions = getFunctions(app)
 connectFunctionsEmulator(functions, '127.0.0.1', 5001)
 export const db = getFirestore(app)
 connectFirestoreEmulator(db, '127.0.0.1', 8080)
+export const storage = getStorage()
+if (location.hostname === 'localhost') {
+  // Point to the Storage emulator running on localhost.
+  connectStorageEmulator(storage, '127.0.0.1', 9199)
+}
