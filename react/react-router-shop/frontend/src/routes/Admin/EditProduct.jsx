17b62497658499d70399f7cc694ad2d0d4d493e0
edo979 <prezentabilan@gmail.com>
2024-01-02 20:21:28 +0100

    style edit product page

--- react/react-router-shop/frontend/src/routes/Admin/EditProduct.jsx
+++ react/react-router-shop/frontend/src/routes/Admin/EditProduct.jsx
@@ -1,9 +1,183 @@
-import { useLoaderData } from 'react-router-dom'
+import { Form, useLoaderData } from 'react-router-dom'
+import classNames from 'classnames'
 
 const EditProduct = () => {
   const { product } = useLoaderData()
+  const errors = undefined
 
-  return <div>EditProduct {product.name}</div>
+  return (
+    <Form method="post" encType="multipart/form-data">
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          className={classNames('form-control', {
+            'is-invalid': errors?.fieldsError?.name,
+          })}
+          aria-describedby={
+            errors?.fieldsError?.name ? 'name-field-error' : undefined
+          }
+          defaultValue={product && product.name}
+          id="name"
+          name="name"
+        />
+        {errors?.fieldsError?.name && (
+          <p id="name-field-error" className="invalid-feedback">
+            {errors.fieldsError.name}
+          </p>
+        )}
+      </div>
+
+      <div className="d-flex gap-1 gap-md-4">
+        <div className="mb-3 w-100">
+          <label htmlFor="brand" className="form-label">
+            Brand
+          </label>
+          <input
+            type="text"
+            className={classNames('form-control', {
+              'is-invalid': errors?.fieldsError?.brand,
+            })}
+            aria-describedby={
+              errors?.fieldsError?.brand ? 'brand-field-error' : undefined
+            }
+            defaultValue={product && product.brand}
+            id="brand"
+            name="brand"
+          />
+          {errors?.fieldsError?.brand && (
+            <p id="brand-field-error" className="invalid-feedback">
+              {errors.fieldsError.brand}
+            </p>
+          )}
+        </div>
+
+        <div className="mb-3 w-100">
+          <label htmlFor="category" className="form-label">
+            Category
+          </label>
+          <input
+            type="text"
+            className={classNames('form-control', {
+              'is-invalid': errors?.fieldsError?.category,
+            })}
+            aria-describedby={
+              errors?.fieldsError?.category ? 'category-field-error' : undefined
+            }
+            defaultValue={product && product.category}
+            id="category"
+            name="category"
+          />
+          {errors?.fieldsError?.category && (
+            <p id="category-field-error" className="invalid-feedback">
+              {errors.fieldsError.category}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
+        <textarea
+          type="text"
+          className={classNames('form-control', {
+            'is-invalid': errors?.fieldsError?.description,
+          })}
+          aria-describedby={
+            errors?.fieldsError?.description
+              ? 'description-field-error'
+              : undefined
+          }
+          defaultValue={product && product.description}
+          id="description"
+          name="description"
+          rows={3}
+        />
+        {errors?.fieldsError?.description && (
+          <p id="description-field-error" className="invalid-feedback">
+            {errors.fieldsError.description}
+          </p>
+        )}
+      </div>
+
+      <div className="d-flex gap-1 gap-md-4">
+        <div className="mb-3 w-100">
+          <label htmlFor="in-stock" className="form-label">
+            In Stock
+          </label>
+          <input
+            type="number"
+            className={classNames('form-control', {
+              'is-invalid': errors?.fieldsError?.inStock,
+            })}
+            aria-describedby={
+              errors?.fieldsError?.inStock ? 'in-stock-field-error' : undefined
+            }
+            defaultValue={product && product.inStock}
+            id="in-stock"
+            name="inStock"
+          />
+          {errors?.fieldsError?.inStock && (
+            <p id="in-stock-field-error" className="invalid-feedback">
+              {errors.fieldsError.inStock}
+            </p>
+          )}
+        </div>
+
+        <div className="mb-3 w-100">
+          <label htmlFor="price" className="form-label">
+            Price
+          </label>
+          <div className="input-group">
+            <span className="input-group-text" id="basic-addon1">
+              $
+            </span>
+            <input
+              type="number"
+              className={classNames('form-control', {
+                'is-invalid': errors?.fieldsError?.price,
+              })}
+              aria-describedby={
+                errors?.fieldsError?.price ? 'price-field-error' : undefined
+              }
+              defaultValue={product && product.price}
+              id="price"
+              name="price"
+              placeholder="9.99"
+              step="0.01"
+            />
+            {errors?.fieldsError?.price && (
+              <p id="price-field-error" className="invalid-feedback">
+                {errors.fieldsError.price}
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="mb-3">
+        <input type="file" name="image" id="image" />
+      </div>
+
+      <div className="d-flex">
+        <button className="btn btn-primary ms-auto">
+          {navigation.state === 'submitting' && (
+            <div
+              className="me-2 spinner-border spinner-border-sm"
+              role="status"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          )}
+          Add Product
+        </button>
+      </div>
+    </Form>
+  )
 }
 
 export default EditProduct
