2d9c2aaea9d315aa992b10d8badb54ceab8a2304
edo979 <prezentabilan@gmail.com>
2024-01-03 22:06:51 +0100

    style error page

--- react/react-router-shop/frontend/src/components/ErrorPage.jsx
+++ react/react-router-shop/frontend/src/components/ErrorPage.jsx
@@ -1,12 +1,30 @@
-import { useRouteError } from 'react-router-dom'
+import { Link, useRouteError } from 'react-router-dom'
 
 const ErrorPage = () => {
   const error = useRouteError()
-
+  console.log(error)
   return (
-    <div>
-      <h1>Sorry Error has happen!</h1>
-      <p>{error.code || error.message}</p>
+    <div className="row">
+      <div className="mt-3 offset-2 col-8">
+        <div className="alert alert-danger shadow-sm">
+          <h1 className="alert-heading text-center">Sorry Error has happen!</h1>
+          <p>
+            <b>{error.message}</b>
+          </p>
+          {import.meta.env.DEV && (
+            <div>
+              <p>{error?.data && error.data}</p>
+              <p>{error?.error?.stack && error.error.stack}</p>
+            </div>
+          )}
+          <hr />
+          <div className="d-flex justify-content-end">
+            <Link to={-1} className="btn btn-danger">
+              Back
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
