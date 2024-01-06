eb7a1f9b666b9d663dd3827de6ed6633f3ca4395
edo979 <prezentabilan@gmail.com>
2024-01-06 10:15:47 +0100

    refactor seed data

--- react/react-router-shop/frontend/sampleData/seeder.js
+++ react/react-router-shop/frontend/sampleData/seeder.js
@@ -1,14 +1,37 @@
-import { db } from '../src/db/init.js'
-import { addDoc, collection } from 'firebase/firestore'
-
+import {
+  addDoc,
+  collection,
+  connectFirestoreEmulator,
+  doc,
+  getFirestore,
+  writeBatch,
+} from 'firebase/firestore'
 import { productsSampleData } from './data.js'
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  projectId: 'demo-app',
+  apiKey: 'any',
+  authDomain: 'any',
+  storageBucket: 'gs://demo-app.appspot.com/',
+}
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+connectFirestoreEmulator(db, '127.0.0.1', 8080)
 
 const collectionName = 'products'
+const batch = writeBatch(db)
 
 const seedData = async () => {
-  productsSampleData.map(async (product) => {
-    await addDoc(collection(db, collectionName), product)
+  productsSampleData.forEach((product) => {
+    const prodRef = doc(collection(db, collectionName))
+    batch.set(prodRef, product)
   })
+
+  await batch.commit()
+  process.exit()
 }
 
 seedData()
