3548bc5651d73c46f4a2e96f65b31b1d85cbe4e3
edo979 <prezentabilan@gmail.com>
2024-01-07 14:00:40 +0100

    refactor error page

--- react/react-router-shop/frontend/src/components/ErrorPage.jsx
+++ react/react-router-shop/frontend/src/components/ErrorPage.jsx
@@ -1,32 +1,10 @@
-import { Link, useRouteError } from 'react-router-dom'
+import { useRouteError } from 'react-router-dom'
+import ErrorContent from './ErrorContent'
 
 const ErrorPage = () => {
   const error = useRouteError()
-  console.log(error)
-  return (
-    <div className="row">
-      <div className="mt-3 offset-2 col-8">
-        <div className="alert alert-danger shadow-sm">
-          <h1 className="alert-heading text-center">Sorry Error has happen!</h1>
-          <p>
-            <b>{error.message}</b>
-          </p>
-          {import.meta.env.DEV && (
-            <div>
-              <p>{error?.data && error.data}</p>
-              <p>{error?.error?.stack && error.error.stack}</p>
-            </div>
-          )}
-          <hr />
-          <div className="d-flex justify-content-end">
-            <Link to={-1} className="btn btn-danger">
-              Back
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+
+  return <ErrorContent error={error} />
 }
 
 export default ErrorPage
--- react/react-router-shop/frontend/src/routes/Cart.jsx
+++ react/react-router-shop/frontend/src/routes/Cart.jsx
@@ -1,6 +1,8 @@
 import { useLoaderData } from 'react-router-dom'
 import { addCartItem, getCartItems } from '../db/cart'
 import { getUser } from '../db/auth'
+import ErrorPage from '../components/ErrorPage'
+import ErrorContent from '../components/ErrorContent'
 
 export async function action({ request }) {
   const productId = (await request.formData()).get('productId')
@@ -16,49 +18,54 @@ export async function action({ request }) {
 }
 
 export async function loader() {
-  // Just wait until auth initialize
-  const user = await getUser()
-
   try {
+    // Just wait until auth initialize
+    const user = await getUser()
+    throw new Error('Jah it is an error!')
     const res = await getCartItems()
     const items = res.data
     console.log(items)
 
     return { items }
   } catch (error) {
-    console.log(error)
-    return { items: [] }
+    return { items: [], error }
   }
 }
 
 const Cart = () => {
-  const { items } = useLoaderData()
+  const { items, error } = useLoaderData()
 
   return (
-    <div className="px-4">
-      <div className="row">
-        <h1>Cart</h1>
-      </div>
-      <div className="row">
-        <div className="col-8">
-          <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4">
-            {items.map((item, i) => (
-              <div className="card" key={i}>
-                <img className="card-img-top" src="" alt="" />
-                <div className="card-body">
-                  <h2 className="card-title fs-5">Title</h2>
-                  <p className="card-text">card text</p>
-                  <p className="card-text">
-                    <small className="text-body-secondary">price</small>
-                  </p>
-                </div>
+    <>
+      {error ? (
+        <ErrorContent error={error} />
+      ) : (
+        <div className="px-4">
+          <div className="row">
+            <h1>Cart</h1>
+          </div>
+          <div className="row">
+            <div className="col-8">
+              <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4">
+                {items.map((item, i) => (
+                  <div className="card" key={i}>
+                    <img className="card-img-top" src="" alt="" />
+                    <div className="card-body">
+                      <h2 className="card-title fs-5">Title</h2>
+                      <p className="card-text">card text</p>
+                      <p className="card-text">
+                        <small className="text-body-secondary">price</small>
+                      </p>
+                    </div>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
+            <div className="col-4">cart list</div>
           </div>
         </div>
-        <div className="col-4">cart list</div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
