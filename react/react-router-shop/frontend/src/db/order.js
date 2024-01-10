8b6871a927579114c59c73b8fefc14ed85698cd9
edo979 <prezentabilan@gmail.com>
2024-01-10 23:01:58 +0100

    refactor rename functions

--- react/react-router-shop/frontend/src/db/order.js
+++ react/react-router-shop/frontend/src/db/order.js
@@ -2,14 +2,14 @@ import { getFunctions, httpsCallable } from 'firebase/functions'
 
 export const createOrder = async (data) => {
   const functions = getFunctions()
-  const createOrder = httpsCallable(functions, 'createOrder')
+  const createOrderFunction = httpsCallable(functions, 'createOrder')
 
-  return await createOrder(data)
+  return await createOrderFunction(data)
 }
 
 export const getOrder = async (id) => {
   const functions = getFunctions()
-  const getOrder = httpsCallable(functions, 'getOrder')
+  const getOrderFunction = httpsCallable(functions, 'getOrder')
 
-  return await getOrder({ id })
+  return await getOrderFunction({ id })
 }
