9f2357723407627eba3d6a9a2cc8bca428b1c19e
edo979 <prezentabilan@gmail.com>
2024-02-22 16:28:27 +0100

    add form

--- remix/expense-tracker/app/components/Modal.tsx
+++ remix/expense-tracker/app/components/Modal.tsx
@@ -14,19 +14,19 @@ function Modal({ children }: PropsWithChildren) {
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title">Modal title</h5>
+              <h5 className="modal-title">Menage Expense</h5>
               <Link to=".." className="btn-close" aria-label="Close" />
             </div>
 
             <div className="modal-body">{children}</div>
 
             <div className="modal-footer">
-              <Link to=".." className="btn btn-secondary">
+              {/* <Link to=".." className="btn btn-secondary">
                 Close
               </Link>
               <button type="button" className="btn btn-primary">
                 Save changes
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
--- remix/expense-tracker/app/routes/expenses.add.tsx
+++ remix/expense-tracker/app/routes/expenses.add.tsx
@@ -3,7 +3,29 @@ import Modal from '~/components/Modal'
 export default function ExpenseAdd() {
   return (
     <Modal>
-      <p>Modal body</p>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Expense title:
+          </label>
+          <input type="text" name="title" id="title" className="form-control" />
+        </div>
+
+        <div className="d-flex align-items-center gap-4">
+          <label htmlFor="amount" className="form-label mb-0">
+            Amount:
+          </label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            className="form-control"
+          />
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
+        </div>
+      </form>
     </Modal>
   )
 }
