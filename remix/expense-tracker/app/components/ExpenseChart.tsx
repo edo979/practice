4fda0d15da387ba2a8c26dc91c87f8eee5144a8b
edo979 <prezentabilan@gmail.com>
2024-02-29 22:22:07 +0100

    refactor chart visuals

--- remix/expense-tracker/app/components/ExpenseChart.tsx
+++ remix/expense-tracker/app/components/ExpenseChart.tsx
@@ -18,18 +18,18 @@ const ExpenseChart = ({ labels, incomes, expenses }: ExpenseChartPropsT) => {
         labels,
         datasets: [
           {
-            label: 'Incomes',
-            data: incomes,
-            backgroundColor: 'rgba(75, 192, 192, 0.45)',
-            borderColor: 'rgb(75, 192, 192)',
+            label: 'Expenses',
+            data: expenses,
+            backgroundColor: 'rgba(255, 99, 132, 0.35)',
+            borderColor: 'rgb(255, 99, 132)',
             borderWidth: 1,
             barPercentage: 0.6,
           },
           {
-            label: 'Expenses',
-            data: expenses,
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            borderColor: 'rgb(255, 99, 132)',
+            label: 'Incomes',
+            data: incomes,
+            backgroundColor: 'rgba(75, 192, 192, 0.45)',
+            borderColor: 'rgb(75, 192, 192)',
             borderWidth: 1,
             barPercentage: 0.6,
           },
@@ -55,22 +55,3 @@ const ExpenseChart = ({ labels, incomes, expenses }: ExpenseChartPropsT) => {
 }
 
 export default ExpenseChart
-
-// backgroundColor: [
-//   'rgba(255, 99, 132, 0.5)',
-//   'rgba(255, 159, 64, 0.5)',
-//   'rgba(255, 205, 86, 0.5)',
-//   'rgba(75, 192, 192, 0.5)',
-//   'rgba(54, 162, 235, 0.5)',
-//   'rgba(153, 102, 255, 0.5)',
-//   'rgba(201, 203, 207, 0.5)',
-// ],
-// borderColor: [
-//   'rgb(255, 99, 132)',
-//   'rgb(255, 159, 64)',
-//   'rgb(255, 205, 86)',
-//   'rgb(75, 192, 192)',
-//   'rgb(54, 162, 235)',
-//   'rgb(153, 102, 255)',
-//   'rgb(201, 203, 207)',
-// ],
